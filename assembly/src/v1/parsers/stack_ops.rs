95268bcd60f12006f74a7699095fee3d8545ef01
Bobbin Threadbare <bobbinth@protonmail.com>
2021-12-15 14:31:50 -0800

    stubbed out all assembly operations

---
+++ assembly/src/v1/parsers/stack_ops.rs
@@ -0,0 +1,82 @@
+use super::{AssemblyError, Operation, Token};
+
+// STACK MANIPULATION
+// ================================================================================================
+
+/// TODO: implement
+pub fn parse_drop(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_dropw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_padw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_dup(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_dupw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_swap(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_swapw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_movup(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_movupw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_movdn(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_movdnw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+// CONDITIONAL MANIPULATION
+// ================================================================================================
+
+/// TODO: implement
+pub fn parse_cswap(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_cswapw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_cdrop(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
+
+/// TODO: implement
+pub fn parse_cdropw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
+    unimplemented!()
+}
