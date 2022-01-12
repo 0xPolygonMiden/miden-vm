d187abce17fa7b23e28d8a08ac18cbffbc4e1b14
Bobbin Threadbare <bobbinth@protonmail.com>
2022-01-12 13:11:35 -0800

    feat: implement processing of stack manipulation ops

--- processor/src/operations/stack_ops.rs
+++ processor/src/operations/stack_ops.rs
@@ -1,4 +1,6 @@
-use super::{BaseElement, ExecutionError, FieldElement, Process};
+use super::{
+    super::STACK_TOP_SIZE, BaseElement, ExecutionError, FieldElement, Process, StarkField,
+};
 
 impl Process {
     // STACK MANIPULATION
@@ -26,111 +28,225 @@ impl Process {
         Ok(())
     }
 
+    /// TODO: add docs
     pub(super) fn op_swap(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
+        self.stack.check_depth(2, "SWAP")?;
+        let a = self.stack.get(0);
+        let b = self.stack.get(1);
+        self.stack.set(0, b);
+        self.stack.set(1, a);
+        self.stack.copy_state(2);
+        Ok(())
     }
 
+    /// TODO: add docs
     pub(super) fn op_swapw(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
+        self.stack.check_depth(8, "SWAPW")?;
+
+        let a0 = self.stack.get(0);
+        let a1 = self.stack.get(1);
+        let a2 = self.stack.get(2);
+        let a3 = self.stack.get(3);
+        let b0 = self.stack.get(4);
+        let b1 = self.stack.get(5);
+        let b2 = self.stack.get(6);
+        let b3 = self.stack.get(7);
+
+        self.stack.set(0, b0);
+        self.stack.set(1, b1);
+        self.stack.set(2, b2);
+        self.stack.set(3, b3);
+        self.stack.set(4, a0);
+        self.stack.set(5, a1);
+        self.stack.set(6, a2);
+        self.stack.set(7, a3);
+
+        self.stack.copy_state(8);
+        Ok(())
     }
 
+    /// TODO: add docs
     pub(super) fn op_swapw2(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
+        self.stack.check_depth(12, "SWAPW2")?;
+
+        let a0 = self.stack.get(0);
+        let a1 = self.stack.get(1);
+        let a2 = self.stack.get(2);
+        let a3 = self.stack.get(3);
+        let b0 = self.stack.get(4);
+        let b1 = self.stack.get(5);
+        let b2 = self.stack.get(6);
+        let b3 = self.stack.get(7);
+        let c0 = self.stack.get(8);
+        let c1 = self.stack.get(9);
+        let c2 = self.stack.get(10);
+        let c3 = self.stack.get(11);
+
+        self.stack.set(0, c0);
+        self.stack.set(1, c1);
+        self.stack.set(2, c2);
+        self.stack.set(3, c3);
+        self.stack.set(4, b0);
+        self.stack.set(5, b1);
+        self.stack.set(6, b2);
+        self.stack.set(7, b3);
+        self.stack.set(8, a0);
+        self.stack.set(9, a1);
+        self.stack.set(10, a2);
+        self.stack.set(11, a3);
+
+        self.stack.copy_state(12);
+        Ok(())
     }
 
+    /// TODO: add docs
     pub(super) fn op_swapw3(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
+        self.stack.check_depth(16, "SWAPW3")?;
+
+        let a0 = self.stack.get(0);
+        let a1 = self.stack.get(1);
+        let a2 = self.stack.get(2);
+        let a3 = self.stack.get(3);
+        let b0 = self.stack.get(4);
+        let b1 = self.stack.get(5);
+        let b2 = self.stack.get(6);
+        let b3 = self.stack.get(7);
+        let c0 = self.stack.get(8);
+        let c1 = self.stack.get(9);
+        let c2 = self.stack.get(10);
+        let c3 = self.stack.get(11);
+        let d0 = self.stack.get(12);
+        let d1 = self.stack.get(13);
+        let d2 = self.stack.get(14);
+        let d3 = self.stack.get(15);
+
+        self.stack.set(0, d0);
+        self.stack.set(1, d1);
+        self.stack.set(2, d2);
+        self.stack.set(3, d3);
+        self.stack.set(4, b0);
+        self.stack.set(5, b1);
+        self.stack.set(6, b2);
+        self.stack.set(7, b3);
+        self.stack.set(8, c0);
+        self.stack.set(9, c1);
+        self.stack.set(10, c2);
+        self.stack.set(11, c3);
+        self.stack.set(12, a0);
+        self.stack.set(13, a1);
+        self.stack.set(14, a2);
+        self.stack.set(15, a3);
+
+        Ok(())
     }
 
-    pub(super) fn op_movup2(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
+    /// TODO: add docs
+    pub(super) fn op_movup(&mut self, n: usize) -> Result<(), ExecutionError> {
+        self.stack.check_depth(n + 1, "MOVUP")?;
+
+        // move the nth value to the top of the stack
+        let value = self.stack.get(n);
+        self.stack.set(0, value);
+
+        // shift all values up to n by one slot to the right
+        for i in 0..n {
+            let value = self.stack.get(i);
+            self.stack.set(i + 1, value);
+        }
+
+        // all other items on the stack remain in place
+        if (n + 1) < STACK_TOP_SIZE {
+            self.stack.copy_state(n + 1);
+        }
+        Ok(())
     }
 
-    pub(super) fn op_movup3(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
+    /// TODO: add docs
+    pub(super) fn op_movdn(&mut self, n: usize) -> Result<(), ExecutionError> {
+        self.stack.check_depth(n + 1, "MOVDN")?;
 
-    pub(super) fn op_movup4(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
+        // move the value at the top of the stack to the nth position
+        let value = self.stack.get(0);
+        self.stack.set(n, value);
 
-    pub(super) fn op_movup5(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
+        // shift all values up to n by one slot to the left
+        for i in 0..n {
+            let value = self.stack.get(i + 1);
+            self.stack.set(i, value);
+        }
 
-    pub(super) fn op_movup6(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movup7(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movup9(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movup11(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movup13(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movup15(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn2(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn3(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn4(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn5(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn6(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn7(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn9(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn11(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn13(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
-    }
-
-    pub(super) fn op_movdn15(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
+        // all other items on the stack remain in place
+        if (n + 1) < STACK_TOP_SIZE {
+            self.stack.copy_state(n + 1);
+        }
+        Ok(())
     }
 
     // CONDITIONAL MANIPULATION
     // --------------------------------------------------------------------------------------------
 
+    /// TODO: add docs
     pub(super) fn op_cswap(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
+        self.stack.check_depth(3, "CSWAP")?;
+        let c = self.stack.get(0);
+        let b = self.stack.get(1);
+        let a = self.stack.get(2);
+
+        match c.as_int() {
+            0 => {
+                self.stack.set(0, b);
+                self.stack.set(1, a);
+            }
+            1 => {
+                self.stack.set(0, a);
+                self.stack.set(1, b);
+            }
+            _ => return Err(ExecutionError::NotBinaryValue(c)),
+        }
+
+        self.stack.shift_left(3);
+        Ok(())
     }
 
+    /// TODO: add docs
     pub(super) fn op_cswapw(&mut self) -> Result<(), ExecutionError> {
-        unimplemented!();
+        self.stack.check_depth(9, "CSWAPW")?;
+        let c = self.stack.get(0);
+        let b0 = self.stack.get(1);
+        let b1 = self.stack.get(2);
+        let b2 = self.stack.get(3);
+        let b3 = self.stack.get(4);
+        let a0 = self.stack.get(5);
+        let a1 = self.stack.get(6);
+        let a2 = self.stack.get(7);
+        let a3 = self.stack.get(8);
+
+        match c.as_int() {
+            0 => {
+                self.stack.set(0, b0);
+                self.stack.set(1, b1);
+                self.stack.set(2, b2);
+                self.stack.set(3, b3);
+                self.stack.set(4, a0);
+                self.stack.set(5, a1);
+                self.stack.set(6, a2);
+                self.stack.set(7, a3);
+            }
+            1 => {
+                self.stack.set(0, a0);
+                self.stack.set(1, a1);
+                self.stack.set(2, a2);
+                self.stack.set(3, a3);
+                self.stack.set(4, b0);
+                self.stack.set(5, b1);
+                self.stack.set(6, b2);
+                self.stack.set(7, b3);
+            }
+            _ => return Err(ExecutionError::NotBinaryValue(c)),
+        }
+
+        self.stack.shift_left(9);
+        Ok(())
     }
 }
 
