4cd2168f3cc1b5c0c70f2000b0192464b86b4398
grjte <grjte@protonmail.com>
2022-01-12 09:45:45 +0000

    test: add assembly crypto op tests for rpperm

--- assembly/src/parsers/crypto_ops.rs
+++ assembly/src/parsers/crypto_ops.rs
@@ -20,3 +20,43 @@ pub fn parse_rpperm(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), A
 pub fn parse_mtree(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
     unimplemented!()
 }
+
+// TESTS
+// ================================================================================================
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rpperm() {
+        let mut span_ops: Vec<Operation> = Vec::new();
+        let op = Token::new("rpperm", 0);
+        let expected = vec![Operation::RpPerm];
+
+        parse_rpperm(&mut span_ops, &op).expect("Failed to parse rpperm");
+
+        assert_eq!(span_ops, expected);
+    }
+
+    #[test]
+    fn rpperm_invalid() {
+        // parse_rpperm should return an error if called with an invalid or incorrect operation
+        let mut span_ops: Vec<Operation> = Vec::new();
+        let op_pos = 0;
+
+        let op_too_long = Token::new("rpperm.12", op_pos);
+        let expected = AssemblyError::extra_param(&op_too_long);
+        assert_eq!(
+            parse_rpperm(&mut span_ops, &op_too_long).unwrap_err(),
+            expected
+        );
+
+        let op_mismatch = Token::new("rphash", op_pos);
+        let expected = AssemblyError::unexpected_token(&op_mismatch, "rpperm");
+        assert_eq!(
+            parse_rpperm(&mut span_ops, &op_mismatch).unwrap_err(),
+            expected
+        );
+    }
+}
