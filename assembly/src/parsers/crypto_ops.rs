a358ae796a2bfad4457c0bc933c98567e6a2dddd
grjte <grjte@protonmail.com>
2022-01-13 08:40:15 +0000

    test: add assembly crypto op tests for rphash

--- assembly/src/parsers/crypto_ops.rs
+++ assembly/src/parsers/crypto_ops.rs
@@ -1,4 +1,4 @@
-use super::{validate_op_len, AssemblyError, Operation, Token};
+use super::{validate_op_len, AssemblyError, BaseElement, Operation, Token};
 
 // HASHING
 // ================================================================================================
@@ -71,6 +71,54 @@ mod tests {
         let expected = AssemblyError::unexpected_token(&op_mismatch, "rpperm");
         assert_eq!(
             parse_rpperm(&mut span_ops, &op_mismatch).unwrap_err(),
+            expected
+        );
+    }
+
+    #[test]
+    fn rphash() {
+        // adds a word to the stack specifying the number of elements to be hashed (8)
+        // does a rescue prime permutation
+        // keeps the top word as the result but drops the other 8 elements
+        let mut span_ops: Vec<Operation> = Vec::new();
+        let op = Token::new("rphash", 0);
+
+        // state of stack before permutation
+        let mut expected = vec![
+            Operation::Pad,
+            Operation::Pad,
+            Operation::Pad,
+            Operation::Push(BaseElement::new(8)),
+        ];
+        // rp permutation leaves stack as [A, B, C,...]
+        expected.push(Operation::RpPerm);
+        // swap A and C, since A is the result we want --> gives [C, B, A, ...]
+        expected.push(Operation::SwapW2);
+        // drop C, B
+        let drop8 = vec![Operation::Drop; 8];
+        expected.extend_from_slice(&drop8);
+
+        parse_rphash(&mut span_ops, &op).expect("Failed to parse rphash");
+        assert_eq!(span_ops, expected);
+    }
+
+    #[test]
+    fn rphash_invalid() {
+        // parse_rphash should return an error if called with an invalid or incorrect operation
+        let mut span_ops: Vec<Operation> = Vec::new();
+        let op_pos = 0;
+
+        let op_too_long = Token::new("rphash.12", op_pos);
+        let expected = AssemblyError::extra_param(&op_too_long);
+        assert_eq!(
+            parse_rphash(&mut span_ops, &op_too_long).unwrap_err(),
+            expected
+        );
+
+        let op_mismatch = Token::new("rpperm", op_pos);
+        let expected = AssemblyError::unexpected_token(&op_mismatch, "rphash");
+        assert_eq!(
+            parse_rphash(&mut span_ops, &op_mismatch).unwrap_err(),
             expected
         );
     }
