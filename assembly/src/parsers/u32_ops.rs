0a997a3c7ecba4ff68e1188945b7ea073fa646be
Timothy Chen <tnachen@gmail.com>
2022-01-17 21:58:07 -0800

    Implement more u32 ops

--- assembly/src/parsers/u32_ops.rs
+++ assembly/src/parsers/u32_ops.rs
@@ -3,7 +3,7 @@ use super::{parse_int_param, AssemblyError, BaseElement, Operation, Token};
 // CONVERSIONS AND TESTS
 // ================================================================================================
 
-/// Translates u32test assembly instruction to VM operation U32SPLIT EQZ.
+/// Translates u32test assembly instruction to VM operation DUP U32SPLIT SWAP DROP EQZ.
 pub fn parse_u32test(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), AssemblyError> {
     match op.num_parts() {
         0 => return Err(AssemblyError::missing_param(op)),
@@ -20,9 +20,47 @@ pub fn parse_u32test(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), As
     Ok(())
 }
 
-/// TODO: implement
-pub fn parse_u32testw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
-    unimplemented!()
+/// Translates u32testw assembly instruction to VM operation
+/// with a series of DUP U32SPLIT SWAP DROP EQZ on each element in the word.
+pub fn parse_u32testw(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), AssemblyError> {
+    match op.num_parts() {
+        0 => return Err(AssemblyError::missing_param(op)),
+        1 => {
+            // Test the fourth element
+            span_ops.push(Operation::Dup3);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Swap);
+            span_ops.push(Operation::Drop);
+            span_ops.push(Operation::Eqz);
+
+            // Test the third element
+            span_ops.push(Operation::Dup3);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Swap);
+            span_ops.push(Operation::Drop);
+            span_ops.push(Operation::Eqz);
+            span_ops.push(Operation::Or);
+
+            // Test the second element
+            span_ops.push(Operation::Dup2);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Swap);
+            span_ops.push(Operation::Drop);
+            span_ops.push(Operation::Eqz);
+            span_ops.push(Operation::Or);
+
+            // Test the first element
+            span_ops.push(Operation::Dup1);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Swap);
+            span_ops.push(Operation::Drop);
+            span_ops.push(Operation::Eqz);
+            span_ops.push(Operation::Or);
+        }
+        _ => return Err(AssemblyError::extra_param(op)),
+    }
+
+    Ok(())
 }
 
 /// Translates u32assert assembly instruction to VM operation U32SPLIT EQZ ASSERT.
@@ -40,9 +78,44 @@ pub fn parse_u32assert(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), 
     Ok(())
 }
 
-/// TODO: implement
-pub fn parse_u32assertw(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
-    unimplemented!()
+/// Translates u32assert assembly instruction to VM operation
+/// into a series of U32SPLIT EQZ ASSERT on each element in the word.
+pub fn parse_u32assertw(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), AssemblyError> {
+    match op.num_parts() {
+        0 => return Err(AssemblyError::missing_param(op)),
+        1 => {
+            // Test the first element
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Eqz);
+            span_ops.push(Operation::Assert);
+
+            // Test the second element
+            span_ops.push(Operation::Swap);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Eqz);
+            span_ops.push(Operation::Assert);
+
+            // Test the third element
+            span_ops.push(Operation::MovUp2);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Eqz);
+            span_ops.push(Operation::Assert);
+
+            // Test the fourth element
+            span_ops.push(Operation::MovUp3);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Eqz);
+            span_ops.push(Operation::Assert);
+
+            // Move the elements back into place
+            span_ops.push(Operation::Swap);
+            span_ops.push(Operation::MovUp2);
+            span_ops.push(Operation::MovUp3);
+        }
+        _ => return Err(AssemblyError::extra_param(op)),
+    }
+
+    Ok(())
 }
 
 /// Translates u32cast assembly instruction to VM operation U32SPLIT DROP.
@@ -259,14 +332,48 @@ pub fn parse_u32shr(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), Ass
     Ok(())
 }
 
-/// TODO: implement
-pub fn parse_u32rotl(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
-    unimplemented!()
+/// Translates u32rotl.x assembly instruction to VM operation PUSH(2^x) MUL U32SPLIT ADD.
+pub fn parse_u32rotl(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), AssemblyError> {
+    match op.num_parts() {
+        0..=1 => return Err(AssemblyError::missing_param(op)),
+        2 => {
+            // Assert the value is u32
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Not);
+            span_ops.push(Operation::Assert);
+
+            let x = parse_int_param(op, 1, 1, 31)?;
+            span_ops.push(Operation::Push(BaseElement::new(2u64.pow(x))));
+            span_ops.push(Operation::Mul);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Add);
+        }
+        _ => return Err(AssemblyError::extra_param(op)),
+    }
+
+    Ok(())
 }
 
-/// TODO: implement
-pub fn parse_u32rotr(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
-    unimplemented!()
+/// Translates u32rotr.x assembly instruction to VM operation PUSH(2^(32-x)) MUL U32SPLIT ADD.
+pub fn parse_u32rotr(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), AssemblyError> {
+    match op.num_parts() {
+        0..=1 => return Err(AssemblyError::missing_param(op)),
+        2 => {
+            // Assert the value is u32
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Not);
+            span_ops.push(Operation::Assert);
+
+            let x = parse_int_param(op, 1, 1, 31)?;
+            span_ops.push(Operation::Push(BaseElement::new(2u64.pow(32 - x))));
+            span_ops.push(Operation::Mul);
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Add);
+        }
+        _ => return Err(AssemblyError::extra_param(op)),
+    }
+
+    Ok(())
 }
 
 /// TODO: implement
@@ -332,9 +439,36 @@ pub fn parse_u32neq(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), Ass
     Ok(())
 }
 
-/// TODO: implement
-pub fn parse_u32lt(_span_ops: &mut Vec<Operation>, _op: &Token) -> Result<(), AssemblyError> {
-    unimplemented!()
+/// Translates u32lt assembly instruction to VM operations.
+/// Specifically we test the first two numbers to be u32 (U32SPLIT NOT ASSERT),
+/// then perform a U32SUB EQZ NOT to check the underflow flag.
+pub fn parse_u32lt(span_ops: &mut Vec<Operation>, op: &Token) -> Result<(), AssemblyError> {
+    match op.num_parts() {
+        0 => return Err(AssemblyError::missing_param(op)),
+        1 => {
+            // Check first number is u32
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Not);
+            span_ops.push(Operation::Assert);
+
+            span_ops.push(Operation::Swap);
+
+            // Check second number is u32
+            span_ops.push(Operation::U32split);
+            span_ops.push(Operation::Not);
+            span_ops.push(Operation::Assert);
+
+            span_ops.push(Operation::Swap);
+
+            span_ops.push(Operation::U32sub);
+            // Check the underflow flag
+            span_ops.push(Operation::Eqz);
+            span_ops.push(Operation::Not);
+        }
+        _ => return Err(AssemblyError::extra_param(op)),
+    }
+
+    Ok(())
 }
 
 /// TODO: implement
