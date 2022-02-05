683533b6823ad3a9bb4b7aa52cc8994af5d9f4e5
Bobbin Threadbare <bobbinth@protonmail.com>
2022-02-04 22:50:17 -0800

    feat: implement hasher trace construction

--- processor/src/hasher/mod.rs
+++ processor/src/hasher/mod.rs
@@ -1,56 +1,74 @@
-use super::{Felt, FieldElement, StarkField, Word};
-use vm_core::hasher::{apply_round, NUM_ROUNDS, STATE_WIDTH};
+use super::{Felt, FieldElement, StarkField, TraceFragment, Word};
+use vm_core::hasher::STATE_WIDTH;
+
+mod selectors;
+
+mod trace;
+use trace::HasherTrace;
+
+// CONSTANTS
+// ================================================================================================
+
+const NUM_SELECTORS: usize = 3;
+
+/// Number of columns in Hasher execution trace. Additional two columns are for row address and
+/// index columns.
+const TRACE_WIDTH: usize = NUM_SELECTORS + STATE_WIDTH + 2;
 
 // TYPE ALIASES
 // ================================================================================================
 
 type HasherState = [Felt; STATE_WIDTH];
+type Selectors = [Felt; NUM_SELECTORS];
 
-// HASHER
+// HASH PROCESSOR
 // ================================================================================================
 
 /// TODO: add docs
-pub struct Hasher {}
+pub struct Hasher {
+    trace: HasherTrace,
+}
 
 impl Hasher {
+    // CONSTRUCTOR
+    // --------------------------------------------------------------------------------------------
     /// TODO: add docs
     pub fn new() -> Self {
-        Self {}
+        Self {
+            trace: HasherTrace::new(),
+        }
     }
+
+    // PUBLIC ACCESSORS
+    // --------------------------------------------------------------------------------------------
+
+    #[allow(dead_code)]
+    pub fn trace_len(&self) -> usize {
+        self.trace.trace_len()
+    }
+
+    // HASHING METHODS
+    // --------------------------------------------------------------------------------------------
 
     /// TODO: add docs
     pub fn permute(&mut self, mut state: HasherState) -> (Felt, HasherState) {
-        for i in 0..NUM_ROUNDS {
-            // TODO: record state into a trace
-            apply_round(&mut state, i);
-        }
+        let addr = self.trace.next_row_addr();
 
-        // TODO: return address of the hash table row
-        (Felt::ZERO, state)
+        self.trace.append_permutation(
+            &mut state,
+            selectors::LINEAR_HASH,
+            selectors::RETURN_STATE,
+            Felt::ZERO,
+        );
+
+        (addr, state)
     }
 
     /// TODO: add docs
     pub fn build_merkle_root(&mut self, value: Word, path: &[Word], index: Felt) -> (Felt, Word) {
-        let mut root = value;
-        let mut index = index.as_int();
-
-        for sibling in path {
-            let mut state = if index & 1 == 0 {
-                build_merge_state(&root, sibling)
-            } else {
-                build_merge_state(sibling, &root)
-            };
-
-            for i in 0..NUM_ROUNDS {
-                // TODO: record state into a trace
-                apply_round(&mut state, i);
-            }
-            root = [state[4], state[5], state[6], state[7]];
-            index >>= 1;
-        }
-
-        // TODO: return address of the hash table row
-        (Felt::ZERO, root)
+        let addr = self.trace.next_row_addr();
+        let root = self.verify_merkle_path(value, path, index.as_int(), MerklePathType::MpVerify);
+        (addr, root)
     }
 
     pub fn update_merkle_root(
@@ -60,54 +78,130 @@ impl Hasher {
         path: &[Word],
         index: Felt,
     ) -> (Felt, Word, Word) {
-        let mut old_root = old_value;
-        let mut new_root = new_value;
-        let mut index = index.as_int();
+        let addr = self.trace.next_row_addr();
+        let index = index.as_int();
 
-        for sibling in path {
-            let (mut old_state, mut new_state) = if index & 1 == 0 {
-                (
-                    build_merge_state(&old_root, sibling),
-                    build_merge_state(&new_root, sibling),
-                )
-            } else {
-                (
-                    build_merge_state(sibling, &old_root),
-                    build_merge_state(sibling, &new_root),
-                )
-            };
+        let old_root = self.verify_merkle_path(old_value, path, index, MerklePathType::MrUpdateOld);
+        let new_root = self.verify_merkle_path(new_value, path, index, MerklePathType::MrUpdateNew);
 
-            for i in 0..NUM_ROUNDS {
-                // TODO: record state into a trace
-                apply_round(&mut old_state, i);
-                apply_round(&mut new_state, i);
+        (addr, old_root, new_root)
+    }
+
+    // HELPER METHODS
+    // --------------------------------------------------------------------------------------------
+
+    fn verify_merkle_path(
+        &mut self,
+        value: Word,
+        path: &[Word],
+        mut index: u64,
+        mp_type: MerklePathType,
+    ) -> Word {
+        let mut root = value;
+
+        let main_selectors = mp_type.main_selectors();
+        let part_selectors = mp_type.part_selectors();
+        const RETURN_HASH: Selectors = selectors::RETURN_HASH;
+
+        if path.len() == 1 {
+            assert!(index <= 1, "");
+            self.verify_mp_leg(root, path[0], &mut index, main_selectors, RETURN_HASH)
+        } else {
+            // first node
+            let sibling = path[0];
+            root = self.verify_mp_leg(root, sibling, &mut index, main_selectors, main_selectors);
+
+            for &sibling in &path[1..path.len() - 1] {
+                root =
+                    self.verify_mp_leg(root, sibling, &mut index, part_selectors, main_selectors);
             }
-            old_root = [old_state[4], old_state[5], old_state[6], old_state[7]];
-            new_root = [new_state[4], new_state[5], new_state[6], new_state[7]];
-            index >>= 1;
-        }
 
-        // TODO: return address of the hash table row
-        (Felt::ZERO, old_root, new_root)
+            // last node
+            let sibling = path[path.len() - 1];
+            self.verify_mp_leg(root, sibling, &mut index, part_selectors, RETURN_HASH)
+        }
+    }
+
+    fn verify_mp_leg(
+        &mut self,
+        root: Word,
+        sibling: Word,
+        index: &mut u64,
+        init_selectors: Selectors,
+        final_selectors: Selectors,
+    ) -> Word {
+        let index_bit = *index & 1;
+        let mut state = build_merge_state(&root, &sibling, index_bit);
+
+        let index_bit = Felt::new(index_bit);
+        self.trace
+            .append_permutation(&mut state, init_selectors, final_selectors, index_bit);
+
+        *index >>= 1;
+        [state[4], state[5], state[6], state[7]]
+    }
+}
+
+// MERKLE PATH TYPE
+// ================================================================================================
+
+enum MerklePathType {
+    MpVerify,
+    MrUpdateOld,
+    MrUpdateNew,
+}
+
+impl MerklePathType {
+    pub fn main_selectors(&self) -> Selectors {
+        match self {
+            Self::MpVerify => selectors::MP_VERIFY,
+            Self::MrUpdateOld => selectors::MR_UPDATE_OLD,
+            Self::MrUpdateNew => selectors::MR_UPDATE_NEW,
+        }
+    }
+
+    pub fn part_selectors(&self) -> Selectors {
+        match self {
+            Self::MpVerify => selectors::CONTINUE_MP_VERIFY,
+            Self::MrUpdateOld => selectors::CONTINUE_MR_UPDATE_OLD,
+            Self::MrUpdateNew => selectors::CONTINUE_MR_UPDATE_NEW,
+        }
     }
 }
 
 // HELPER FUNCTIONS
 // ================================================================================================
 
-fn build_merge_state(a: &Word, b: &Word) -> HasherState {
-    [
-        Felt::new(8),
-        Felt::ZERO,
-        Felt::ZERO,
-        Felt::ZERO,
-        a[0],
-        a[1],
-        a[2],
-        a[3],
-        b[0],
-        b[1],
-        b[2],
-        b[3],
-    ]
+fn build_merge_state(a: &Word, b: &Word, index_bit: u64) -> HasherState {
+    match index_bit {
+        0 => [
+            Felt::new(8),
+            Felt::ZERO,
+            Felt::ZERO,
+            Felt::ZERO,
+            a[0],
+            a[1],
+            a[2],
+            a[3],
+            b[0],
+            b[1],
+            b[2],
+            b[3],
+        ],
+        1 => [
+            Felt::new(8),
+            Felt::ZERO,
+            Felt::ZERO,
+            Felt::ZERO,
+            b[0],
+            b[1],
+            b[2],
+            b[3],
+            a[0],
+            a[1],
+            a[2],
+            a[3],
+        ],
+        _ => panic!(""),
+    }
 }
---
+++ processor/src/hasher/trace.rs
@@ -0,0 +1,103 @@
+use super::{Felt, FieldElement, HasherState, Selectors, TraceFragment, TRACE_WIDTH};
+use vm_core::hasher::{apply_round, NUM_ROUNDS, STATE_WIDTH};
+
+// HASHER TRACE
+// ================================================================================================
+
+/// TODO: add docs
+pub struct HasherTrace {
+    selectors: [Vec<Felt>; 3],
+    row_addr: Vec<Felt>,
+    hasher_state: [Vec<Felt>; STATE_WIDTH],
+    node_index: Vec<Felt>,
+}
+
+impl HasherTrace {
+    // CONSTRUCTOR
+    // --------------------------------------------------------------------------------------------
+    /// TODO: add docs
+    pub fn new() -> Self {
+        let state = (0..STATE_WIDTH).map(|_| Vec::new()).collect::<Vec<_>>();
+
+        Self {
+            selectors: [Vec::new(), Vec::new(), Vec::new()],
+            row_addr: Vec::new(),
+            hasher_state: state.try_into().expect("failed to convert vector to array"),
+            node_index: Vec::new(),
+        }
+    }
+
+    // PUBLIC ACCESSORS
+    // --------------------------------------------------------------------------------------------
+
+    /// Returns current length of this execution trace.
+    pub fn trace_len(&self) -> usize {
+        self.row_addr.len()
+    }
+
+    /// Returns next row address. The address is equal to the current trace length.
+    pub fn next_row_addr(&self) -> Felt {
+        Felt::new(self.trace_len() as u64)
+    }
+
+    // TRACE MUTATORS
+    // --------------------------------------------------------------------------------------------
+
+    /// Appends 8 rows to the execution trace describing a single permutation of the hash function.
+    ///
+    /// TODO: add detail
+    pub fn append_permutation(
+        &mut self,
+        state: &mut HasherState,
+        init_selectors: Selectors,
+        final_selectors: Selectors,
+        index: Felt,
+    ) {
+        self.append_row(init_selectors, state, index);
+
+        let next_selectors = [Felt::ZERO, init_selectors[1], init_selectors[2]];
+        for i in 0..NUM_ROUNDS - 1 {
+            apply_round(state, i);
+            self.append_row(next_selectors, state, index);
+        }
+
+        apply_round(state, NUM_ROUNDS - 1);
+        self.append_row(final_selectors, state, index);
+    }
+
+    /// Appends a new row to the execution trace based on the supplied parameters.
+    fn append_row(&mut self, selectors: Selectors, state: &HasherState, index: Felt) {
+        self.row_addr.push(self.next_row_addr());
+        for (trace_col, selector_val) in self.selectors.iter_mut().zip(selectors) {
+            trace_col.push(selector_val);
+        }
+        for (trace_col, &state_val) in self.hasher_state.iter_mut().zip(state) {
+            trace_col.push(state_val);
+        }
+        self.node_index.push(index);
+    }
+
+    // EXECUTION TRACE GENERATION
+    // --------------------------------------------------------------------------------------------
+
+    /// Fills the provide trace fragment with trace data from this bitwise helper instance.
+    #[allow(dead_code)]
+    pub fn fill_trace(self, trace: &mut TraceFragment) {
+        // make sure fragment dimensions are consistent with the dimensions of this trace
+        debug_assert_eq!(self.trace_len(), trace.len(), "inconsistent trace lengths");
+        debug_assert_eq!(TRACE_WIDTH, trace.width(), "inconsistent trace widths");
+
+        // collect all trace columns into a single vector
+        let mut columns = Vec::new();
+        self.selectors.into_iter().for_each(|c| columns.push(c));
+        columns.push(self.row_addr);
+        self.hasher_state.into_iter().for_each(|c| columns.push(c));
+        columns.push(self.node_index);
+
+        // copy trace into the fragment column-by-column
+        // TODO: this can be parallelized to copy columns in multiple threads
+        for (out_column, column) in trace.columns().zip(columns) {
+            out_column.copy_from_slice(&column);
+        }
+    }
+}
