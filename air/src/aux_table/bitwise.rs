32c659544bec39952f2ff009835b7016edaa4e32
grjte <grjte@protonmail.com>
2022-05-09 17:53:26 +0100

    docs: add docs for bitwise air constraints

--- air/src/aux_table/bitwise.rs
+++ air/src/aux_table/bitwise.rs
@@ -22,11 +22,17 @@ pub const PERIODIC_CONSTRAINTS_START: usize = 2;
 /// The number of bits decomposed per row.
 const NUM_DECOMP_BITS: usize = 4;
 
+/// The range of the selector columns in the trace.
 const SELECTOR_COL_RANGE: Range<usize> = create_range(BITWISE_TRACE_OFFSET, NUM_SELECTORS);
+/// The index of the column holding the aggregated value of input `a`.
 const A_COL_IDX: usize = SELECTOR_COL_RANGE.end;
+/// The index of the column holding the aggregated value of input `b`.
 const B_COL_IDX: usize = A_COL_IDX + 1;
+/// The index range for the bit decomposition of `a`.
 const A_COL_RANGE: Range<usize> = create_range(B_COL_IDX + 1, NUM_DECOMP_BITS);
+/// The index range for the bit decomposition of `b`.
 const B_COL_RANGE: Range<usize> = create_range(A_COL_RANGE.end, NUM_DECOMP_BITS);
+/// The index of the column containing the aggregated output value.
 const OUTPUT_COL_IDX: usize = B_COL_RANGE.end;
 
 // BITWISE TRANSITION CONSTRAINTS
@@ -74,6 +80,14 @@ pub fn enforce_constraints<E: FieldElement>(
 
 // TRANSITION CONSTRAINT HELPERS
 // ================================================================================================
+
+/// Enforces correct decomposition of the input values `a` and `b` in each row. This requires the
+/// following constraints:
+/// - All values in decomposition columns must be binary.
+/// - In the first row, the values in `a` and `b` must be the aggregation of their respective bit
+///   columns.
+/// - For every row except the last, the aggregated input value in the next row must be 16 times the
+///   the value in the current row plus the aggregation of the bit decomposition in the next row.
 fn enforce_input_decomposition<E: FieldElement>(
     frame: &EvaluationFrame<E>,
     periodic_values: &[E],
@@ -114,6 +128,15 @@ fn enforce_input_decomposition<E: FieldElement>(
     constraint_count
 }
 
+/// Enforces correct output aggregation for the operation. This requires the following 2 constraints
+/// for each operation:
+/// - In the first row, the output value must equal the aggregated result of the operation applied
+///   to the row.
+/// - For every row except the last, the output value in the next row must equal the output in the
+///   current row plus the aggregated result of the specified operation applied to the next row.
+///
+/// Because the selectors for the AND, OR, and XOR operations are mutually exclusive, the
+/// constraints for different operations can be aggregated into the same result indices.
 fn enforce_output_aggregation<E: FieldElement>(
     frame: &EvaluationFrame<E>,
     periodic_values: &[E],
@@ -148,7 +171,7 @@ fn enforce_output_aggregation<E: FieldElement>(
     );
 
     // During a transition between rows, the next value in the output column should be 16 times the
-    // previous value plus the aggregation of the nevt row's operation output.
+    // previous value plus the aggregation of the next row's operation output.
     let shifted_output = frame.output() * E::from(16_u8);
     result.agg_constraint(
         1,
@@ -169,6 +192,8 @@ fn enforce_output_aggregation<E: FieldElement>(
     constraint_count
 }
 
+/// Calculates the result of bitwise AND applied to the decomposed values provided as a bit array.
+/// The result will be the AND of the first 4 bits in the provided array with the latter 4 bits.
 fn bitwise_and<E: FieldElement>(decomposed_values: &[E]) -> E {
     let mut result = E::ZERO;
     // Aggregate the result of the bitwise AND over the decomposed bits in the row.
@@ -180,6 +205,8 @@ fn bitwise_and<E: FieldElement>(decomposed_values: &[E]) -> E {
     result
 }
 
+/// Calculates the result of bitwise OR applied to the decomposed values provided as a bit array.
+/// The result will be the OR of the first 4 bits in the provided array with the latter 4 bits.
 fn bitwise_or<E: FieldElement>(decomposed_values: &[E]) -> E {
     let mut result = E::ZERO;
     // Aggregate the result of the bitwise OR over the decomposed bits in the row.
@@ -190,6 +217,9 @@ fn bitwise_or<E: FieldElement>(decomposed_values: &[E]) -> E {
     }
     result
 }
+
+/// Calculates the result of bitwise XOR applied to the decomposed values provided as a bit array.
+/// The result will be the XOR of the first 4 bits in the provided array with the latter 4 bits.
 fn bitwise_xor<E: FieldElement>(decomposed_values: &[E]) -> E {
     let mut result = E::ZERO;
     // Aggregate the result of the bitwise XOR over the decomposed bits in the row.
@@ -206,29 +236,48 @@ fn bitwise_xor<E: FieldElement>(decomposed_values: &[E]) -> E {
 trait EvaluationFrameExt<E: FieldElement> {
     // --- Column accessors -----------------------------------------------------------------------
 
+    /// Gets the current value of the specified selector column.
     fn selector(&self, index: usize) -> E;
+    /// Gets the current value of the aggregated `a` input.
     fn a(&self) -> E;
+    /// Gets the value of the aggregated `a` input in the next row.
     fn a_next(&self) -> E;
+    /// Gets the value of the decomposed bit of `a` at the specified index in the current row.
     fn a_bit(&self, index: usize) -> E;
+    /// Gets the current value of the aggregated `b` input.
     fn b(&self) -> E;
+    /// Gets the value of the aggregated `b` input in the next row.
     fn b_next(&self) -> E;
+    /// Gets the value of the decomposed bit of `b` at the specified index in the current row.
     fn b_bit(&self, index: usize) -> E;
+    /// Gets the entire range of decomposed input values for `a` and `b` in the current row.
     fn bit_decomp(&self) -> &[E];
+    /// Gets the entire range of decomposed input values for `a` and `b` in the next row.
     fn bit_decomp_next(&self) -> &[E];
+    /// Gets the value of the aggregated output in the current row.
     fn output(&self) -> E;
+    /// Gets the value of the aggregated output in the next row.
     fn output_next(&self) -> E;
 
     // --- Intermediate variables & helpers -------------------------------------------------------
+    /// The aggregated value of the decomposed bits from `a` in the current row.
     fn a_agg_bits(&self) -> E;
+    /// The aggregated value of the decomposed bits from `a` in the next row.
     fn a_agg_bits_next(&self) -> E;
+    /// The aggregated value of the decomposed bits from `b` in the current row.
     fn b_agg_bits(&self) -> E;
+    /// The aggregated value of the decomposed bits from `b` in the next row.
     fn b_agg_bits_next(&self) -> E;
 
     // --- Flags ----------------------------------------------------------------------------------
 
+    /// A selector flag that specifies the operation is any one of bitwise AND, OR or XOR.
     fn bitwise_op_flag(&self) -> E;
+    /// The selector flag for the bitwise AND operation.
     fn bitwise_and_flag(&self) -> E;
+    /// The selector flag for the bitwise OR operation.
     fn bitwise_or_flag(&self) -> E;
+    /// The selector flag for the bitwise XOR operation.
     fn bitwise_xor_flag(&self) -> E;
 }
 
@@ -301,6 +350,8 @@ impl<E: FieldElement> EvaluationFrameExt<E> for &EvaluationFrame<E> {
 
 // HELPER FUNCTIONS
 // ================================================================================================
+/// Aggregate 4 decomposed bits representing a 4-bit binary value into a decimal value, starting
+/// from `start_idx` in the provided row.
 fn agg_bits<E: FieldElement>(row: &[E], start_idx: usize) -> E {
     let mut result = E::ZERO;
     // TODO: this can be optimized.
