66043d77f4ef40084e26db6e926e69233e9b7121
grjte <grjte@protonmail.com>
2022-06-28 17:07:53 +0100

    test(proc): minor cleanup refactor of memory unit tests
    - switch to ZERO/ONE from Felt::ZERO/Felt::ONE
    - replace hard-coded trace width with constant

--- processor/src/memory/tests.rs
+++ processor/src/memory/tests.rs
@@ -3,7 +3,9 @@ use crate::{
     utils::split_u32_into_u16,
 };
 
-use super::{Felt, FieldElement, Memory, MemoryTrace, StarkField, Word};
+use super::{
+    Felt, FieldElement, Memory, MemoryTrace, StarkField, Word, MEMORY_TRACE_WIDTH, ONE, ZERO,
+};
 
 #[test]
 fn mem_init() {
@@ -20,7 +22,7 @@ fn mem_read() {
     mem.advance_clock();
     let addr0 = Felt::new(0);
     let value = mem.read(addr0);
-    assert_eq!([Felt::ZERO; 4], value);
+    assert_eq!([ZERO; 4], value);
     assert_eq!(1, mem.size());
     assert_eq!(1, mem.trace_len());
 
@@ -28,14 +30,14 @@ fn mem_read() {
     mem.advance_clock();
     let addr3 = Felt::new(3);
     let value = mem.read(addr3);
-    assert_eq!([Felt::ZERO; 4], value);
+    assert_eq!([ZERO; 4], value);
     assert_eq!(2, mem.size());
     assert_eq!(2, mem.trace_len());
 
     // read a value from address 0 again; clk = 3
     mem.advance_clock();
     let value = mem.read(addr0);
-    assert_eq!([Felt::ZERO; 4], value);
+    assert_eq!([ZERO; 4], value);
     assert_eq!(2, mem.size());
     assert_eq!(3, mem.trace_len());
 
@@ -43,7 +45,7 @@ fn mem_read() {
     mem.advance_clock();
     let addr2 = Felt::new(2);
     let value = mem.read(addr2);
-    assert_eq!([Felt::ZERO; 4], value);
+    assert_eq!([ZERO; 4], value);
     assert_eq!(3, mem.size());
     assert_eq!(4, mem.trace_len());
 
@@ -54,21 +56,21 @@ fn mem_read() {
     } = mem.into_trace();
 
     // address 0
-    let expected = build_trace_row(addr0, 1, [Felt::ZERO; 4], [Felt::ZERO; 4], [Felt::ZERO; 14]);
+    let expected = build_trace_row(addr0, 1, [ZERO; 4], [ZERO; 4], [ZERO; MEMORY_TRACE_WIDTH]);
     let mut expected_deltas = vec![0]; // clk delta: 1 - 0 - 1
     assert_eq!(expected, read_trace_row(&trace, 0));
 
-    let expected = build_trace_row(addr0, 3, [Felt::ZERO; 4], [Felt::ZERO; 4], expected);
+    let expected = build_trace_row(addr0, 3, [ZERO; 4], [ZERO; 4], expected);
     expected_deltas.push(1); // clk delta: 3 - 1 - 1
     assert_eq!(expected, read_trace_row(&trace, 1));
 
     // address 2
-    let expected = build_trace_row(addr2, 4, [Felt::ZERO; 4], [Felt::ZERO; 4], expected);
+    let expected = build_trace_row(addr2, 4, [ZERO; 4], [ZERO; 4], expected);
     expected_deltas.push(2); // addr delta: addr2 - addr0
     assert_eq!(expected, read_trace_row(&trace, 2));
 
     // address 3
-    let expected = build_trace_row(addr3, 2, [Felt::ZERO; 4], [Felt::ZERO; 4], expected);
+    let expected = build_trace_row(addr3, 2, [ZERO; 4], [ZERO; 4], expected);
     expected_deltas.push(1); // addr delta: addr3 - addr2
     assert_eq!(expected, read_trace_row(&trace, 3));
 
@@ -84,7 +86,7 @@ fn mem_write() {
     // write a value into address 0; clk = 1
     mem.advance_clock();
     let addr0 = Felt::new(0);
-    let value1 = [Felt::ONE, Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value1 = [ONE, ZERO, ZERO, ZERO];
     mem.write(addr0, value1);
     assert_eq!(value1, mem.get_value(addr0.as_int()).unwrap());
     assert_eq!(1, mem.size());
@@ -93,7 +95,7 @@ fn mem_write() {
     // write a value into address 2; clk = 2
     mem.advance_clock();
     let addr2 = Felt::new(2);
-    let value5 = [Felt::new(5), Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value5 = [Felt::new(5), ZERO, ZERO, ZERO];
     mem.write(addr2, value5);
     assert_eq!(value5, mem.get_value(addr2.as_int()).unwrap());
     assert_eq!(2, mem.size());
@@ -102,7 +104,7 @@ fn mem_write() {
     // write a value into address 1; clk = 3
     mem.advance_clock();
     let addr1 = Felt::new(1);
-    let value7 = [Felt::new(7), Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value7 = [Felt::new(7), ZERO, ZERO, ZERO];
     mem.write(addr1, value7);
     assert_eq!(value7, mem.get_value(addr1.as_int()).unwrap());
     assert_eq!(3, mem.size());
@@ -110,7 +112,7 @@ fn mem_write() {
 
     // write a value into address 0; clk = 4
     mem.advance_clock();
-    let value9 = [Felt::new(9), Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value9 = [Felt::new(9), ZERO, ZERO, ZERO];
     mem.write(addr0, value9);
     assert_eq!(value7, mem.get_value(addr1.as_int()).unwrap());
     assert_eq!(3, mem.size());
@@ -123,7 +125,7 @@ fn mem_write() {
     } = mem.into_trace();
 
     // address 0
-    let expected = build_trace_row(addr0, 1, [Felt::ZERO; 4], value1, [Felt::ZERO; 14]);
+    let expected = build_trace_row(addr0, 1, [ZERO; 4], value1, [ZERO; MEMORY_TRACE_WIDTH]);
     let mut expected_deltas = vec![0]; // clk delta: 1 - 0 - 1
     assert_eq!(expected, read_trace_row(&trace, 0));
 
@@ -132,12 +134,12 @@ fn mem_write() {
     assert_eq!(expected, read_trace_row(&trace, 1));
 
     // address 1
-    let expected = build_trace_row(addr1, 3, [Felt::ZERO; 4], value7, expected);
+    let expected = build_trace_row(addr1, 3, [ZERO; 4], value7, expected);
     expected_deltas.push(1); // addr delta: addr1 - addr0
     assert_eq!(expected, read_trace_row(&trace, 2));
 
     // address 2
-    let expected = build_trace_row(addr2, 2, [Felt::ZERO; 4], value5, expected);
+    let expected = build_trace_row(addr2, 2, [ZERO; 4], value5, expected);
     expected_deltas.push(1); // addr delta: addr2 - addr1
     assert_eq!(expected, read_trace_row(&trace, 3));
 
@@ -153,13 +155,13 @@ fn mem_write_read() {
     // write 1 into address 5; clk = 1
     mem.advance_clock();
     let addr5 = Felt::new(5);
-    let value1 = [Felt::ONE, Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value1 = [ONE, ZERO, ZERO, ZERO];
     mem.write(addr5, value1);
 
     // write 4 into address 2; clk = 2
     mem.advance_clock();
     let addr2 = Felt::new(2);
-    let value4 = [Felt::new(4), Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value4 = [Felt::new(4), ZERO, ZERO, ZERO];
     mem.write(addr2, value4);
 
     // read a value from address 5; clk = 3
@@ -168,7 +170,7 @@ fn mem_write_read() {
 
     // write 2 into address 5; clk = 4
     mem.advance_clock();
-    let value2 = [Felt::new(2), Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value2 = [Felt::new(2), ZERO, ZERO, ZERO];
     mem.write(addr5, value2);
 
     // read a value from address 2; clk = 5
@@ -177,7 +179,7 @@ fn mem_write_read() {
 
     // write 7 into address 2; clk = 6
     mem.advance_clock();
-    let value7 = [Felt::new(7), Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value7 = [Felt::new(7), ZERO, ZERO, ZERO];
     mem.write(addr2, value7);
 
     // read a value from address 5; clk = 7
@@ -199,7 +201,7 @@ fn mem_write_read() {
     } = mem.into_trace();
 
     // address 2
-    let expected = build_trace_row(addr2, 2, [Felt::ZERO; 4], value4, [Felt::ZERO; 14]);
+    let expected = build_trace_row(addr2, 2, [ZERO; 4], value4, [ZERO; MEMORY_TRACE_WIDTH]);
     let mut expected_deltas = vec![0]; // clk delta: 2 - 1 - 1
     assert_eq!(expected, read_trace_row(&trace, 0));
 
@@ -216,7 +218,7 @@ fn mem_write_read() {
     assert_eq!(expected, read_trace_row(&trace, 3));
 
     // address 5
-    let expected = build_trace_row(addr5, 1, [Felt::ZERO; 4], value1, expected);
+    let expected = build_trace_row(addr5, 1, [ZERO; 4], value1, expected);
     expected_deltas.push(3); // addr delta: 5 - 2
     assert_eq!(expected, read_trace_row(&trace, 4));
 
@@ -249,14 +251,14 @@ fn mem_get_values_at() {
     // This means that mem[5] = 1 at the beginning of `clk=2`
     mem.advance_clock();
     let addr5 = Felt::new(5);
-    let value1 = [Felt::ONE, Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value1 = [ONE, ZERO, ZERO, ZERO];
     mem.write(addr5, value1);
 
     // Write 4 into address 2 at clk = 2.
     // This means that mem[4] = 2 at the beginning of `clk=3`
     mem.advance_clock();
     let addr2 = Felt::new(2);
-    let value4 = [Felt::new(4), Felt::ZERO, Felt::ZERO, Felt::ZERO];
+    let value4 = [Felt::new(4), ZERO, ZERO, ZERO];
     mem.write(addr2, value4);
 
     mem.advance_clock();
@@ -280,8 +282,8 @@ fn mem_get_values_at() {
 // HELPER FUNCTIONS
 // ================================================================================================
 
-fn read_trace_row(trace: &[Vec<Felt>], step: usize) -> [Felt; 14] {
-    let mut row = [Felt::ZERO; 14];
+fn read_trace_row(trace: &[Vec<Felt>], step: usize) -> [Felt; MEMORY_TRACE_WIDTH] {
+    let mut row = [ZERO; MEMORY_TRACE_WIDTH];
     for (value, column) in row.iter_mut().zip(trace) {
         *value = column[step];
     }
@@ -293,10 +295,10 @@ fn build_trace_row(
     clk: u64,
     old_val: Word,
     new_val: Word,
-    prev_row: [Felt; 14],
-) -> [Felt; 14] {
-    let mut row = [Felt::ZERO; 14];
-    row[0] = Felt::ZERO; // ctx
+    prev_row: [Felt; MEMORY_TRACE_WIDTH],
+) -> [Felt; MEMORY_TRACE_WIDTH] {
+    let mut row = [ZERO; MEMORY_TRACE_WIDTH];
+    row[0] = ZERO; // ctx
     row[1] = addr;
     row[2] = Felt::new(clk);
     row[3] = old_val[0];
@@ -308,13 +310,13 @@ fn build_trace_row(
     row[9] = new_val[2];
     row[10] = new_val[3];
 
-    if prev_row != [Felt::ZERO; 14] {
+    if prev_row != [ZERO; MEMORY_TRACE_WIDTH] {
         let delta = if row[0] != prev_row[0] {
             row[0] - prev_row[0]
         } else if row[1] != prev_row[1] {
             row[1] - prev_row[1]
         } else {
-            row[2] - prev_row[2] - Felt::ONE
+            row[2] - prev_row[2] - ONE
         };
 
         let (hi, lo) = super::split_element_u32_into_u16(delta);
