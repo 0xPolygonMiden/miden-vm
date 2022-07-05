70259eced68ade5c057c512ebfc13e050e928bb2
grjte <grjte@protonmail.com>
2022-07-05 17:35:55 +0100

    feat(air): update constraints for new aux table order

--- air/src/aux_table/mod.rs
+++ air/src/aux_table/mod.rs
@@ -1,5 +1,5 @@
 use super::{Assertion, EvaluationFrame, Felt, FieldElement, TransitionConstraintDegree, Vec};
-use crate::utils::{binary_not, is_binary};
+use crate::utils::{are_equal, binary_not, is_binary};
 use vm_core::{
     aux_table::{BITWISE_TRACE_OFFSET, HASHER_TRACE_OFFSET},
     AUX_TABLE_OFFSET,
@@ -16,21 +16,13 @@ mod memory;
 pub const NUM_ASSERTIONS: usize = hasher::NUM_ASSERTIONS;
 /// The number of constraints on the management of the Auxiliary Table. This does not include
 /// constraints for the co-processors.
-pub const NUM_CONSTRAINTS: usize = 3;
+pub const NUM_CONSTRAINTS: usize = 6;
 /// The degrees of constraints on the management of the Auxiliary Table. This does not include
 /// constraint degrees for the co-processors
 pub const CONSTRAINT_DEGREES: [usize; NUM_CONSTRAINTS] = [
     2, 3, 4, // Selector flags must be binary.
+    2, 3, 4, // Selector flags can only change from 0 -> 1.
 ];
-
-/// The first selector column, used as a selector for the entire auxiliary table.
-pub const S0_COL_IDX: usize = AUX_TABLE_OFFSET;
-/// The second selector column, used as a selector for the bitwise, memory, and padding segments
-/// after the hasher trace ends.
-pub const S1_COL_IDX: usize = AUX_TABLE_OFFSET + 1;
-/// The third selector column, used as a selector for the memory and padding segments after the
-/// bitwise trace ends.
-pub const S2_COL_IDX: usize = AUX_TABLE_OFFSET + 2;
 
 // PERIODIC COLUMNS
 // ================================================================================================
@@ -91,7 +83,7 @@ pub fn enforce_constraints<E: FieldElement<BaseField = Felt>>(
         &periodic_values[..hasher::NUM_PERIODIC_COLUMNS],
         &mut result[constraint_offset..],
         frame.hasher_flag(),
-        binary_not(frame.s0_next()),
+        binary_not(frame.s_next(0)),
     );
     constraint_offset += hasher::get_transition_constraint_count();
 
@@ -105,7 +97,11 @@ pub fn enforce_constraints<E: FieldElement<BaseField = Felt>>(
     constraint_offset += bitwise_pow2::get_transition_constraint_count();
 
     // memory transition constraints
-    memory::enforce_constraints(frame, &mut result[constraint_offset..], frame.memory_flag());
+    memory::enforce_constraints(
+        frame,
+        &mut result[constraint_offset..],
+        frame.memory_flag(false),
+    );
 }
 
 // TRANSITION CONSTRAINT HELPERS
@@ -114,14 +110,27 @@ pub fn enforce_constraints<E: FieldElement<BaseField = Felt>>(
 /// Constraint evaluation function to enforce that the Auxiliary Table selector columns must be
 /// binary during the portion of the trace when they're being used as selectors.
 fn enforce_selectors<E: FieldElement>(frame: &EvaluationFrame<E>, result: &mut [E]) {
+    // --- Selector flags must be binary ----------------------------------------------------------
+
     // Selector flag s0 must be binary for the entire table.
-    result[0] = is_binary(frame.s0());
+    result[0] = is_binary(frame.s(0));
 
-    // Selector s1 is only used as a flag when s0 is set.
-    result[1] = frame.s0() * is_binary(frame.s1());
+    // When s0 is set, selector s1 is binary.
+    result[1] = frame.s(0) * is_binary(frame.s(1));
 
-    // Selector s2 is only used as a flag when both s0 and s1 are set.
-    result[2] = frame.s0() * frame.s1() * is_binary(frame.s2());
+    // When selectors s0 and s1 are set, s2 is binary.
+    result[2] = frame.s(0) * frame.s(1) * is_binary(frame.s(2));
+
+    // --- Selector flags can only stay the same or change from 0 -> 1 ----------------------------
+
+    // Selector flag s0 must either be 0 in the current row or 1 in both rows.
+    result[3] = frame.s(0) * are_equal(frame.s(0), frame.s_next(0));
+
+    // When s0 is set, selector flag s1 must either be 0 in the current row or 1 in both rows.
+    result[4] = frame.s(0) * frame.s(1) * are_equal(frame.s(1), frame.s_next(1));
+
+    // When selectors s0 and s1 are set, s2 must either be 0 in the current row or 1 in both rows.
+    result[5] = frame.s(0) * frame.s(1) * frame.s(2) * are_equal(frame.s(2), frame.s_next(2));
 }
 
 // AUXILIARY TABLE FRAME EXTENSION TRAIT
@@ -132,17 +141,13 @@ fn enforce_selectors<E: FieldElement>(frame: &EvaluationFrame<E>, result: &mut [
 trait EvaluationFrameExt<E: FieldElement> {
     // --- Column accessors -----------------------------------------------------------------------
 
-    /// Current value of the S0 selector column.
-    fn s0(&self) -> E;
+    /// Returns the current value of the specified selector column. It assumes that the index is a
+    /// valid selector index.
+    fn s(&self, idx: usize) -> E;
 
-    /// Value of the S0 selector column in the next row.
-    fn s0_next(&self) -> E;
-
-    /// Current value of the S1 selector column.
-    fn s1(&self) -> E;
-
-    /// Current value of the S2 selector column.
-    fn s2(&self) -> E;
+    /// Returns the value of the specified selector column at the next row. It assumes that the
+    /// index is a valid selector index.
+    fn s_next(&self, idx: usize) -> E;
 
     // --- Co-processor selector flags ------------------------------------------------------------
 
@@ -153,41 +158,40 @@ trait EvaluationFrameExt<E: FieldElement> {
     fn bitwise_flag(&self) -> E;
 
     /// Flag to indicate whether the frame is in the memory portion of the Auxiliary Table trace.
-    fn memory_flag(&self) -> E;
+    /// When `include_last_row` is true, the memory flag is true for every row where the memory
+    /// selectors are set. When false, the last row is excluded. When this flag is used for
+    /// transition constraints with `include_last_row = false`, they will not be applied to the
+    /// final row of the memory trace.
+    fn memory_flag(&self, include_last_row: bool) -> E;
 }
 
 impl<E: FieldElement> EvaluationFrameExt<E> for &EvaluationFrame<E> {
     // --- Column accessors -----------------------------------------------------------------------
 
-    #[inline(always)]
-    fn s0(&self) -> E {
-        self.current()[S0_COL_IDX]
+    fn s(&self, idx: usize) -> E {
+        self.current()[AUX_TABLE_OFFSET + idx]
     }
-    #[inline(always)]
-    fn s0_next(&self) -> E {
-        self.next()[S0_COL_IDX]
-    }
-    #[inline(always)]
-    fn s1(&self) -> E {
-        self.current()[S1_COL_IDX]
-    }
-    #[inline(always)]
-    fn s2(&self) -> E {
-        self.current()[S2_COL_IDX]
+
+    fn s_next(&self, idx: usize) -> E {
+        self.next()[AUX_TABLE_OFFSET + idx]
     }
 
     // --- Co-processor selector flags ------------------------------------------------------------
 
     #[inline(always)]
     fn hasher_flag(&self) -> E {
-        binary_not(self.s0())
+        binary_not(self.s(0))
     }
     #[inline(always)]
     fn bitwise_flag(&self) -> E {
-        self.s0() * binary_not(self.s1())
+        self.s(0) * binary_not(self.s_next(1))
     }
     #[inline(always)]
-    fn memory_flag(&self) -> E {
-        self.s0() * self.s1() * self.s2()
+    fn memory_flag(&self, include_last_row: bool) -> E {
+        if include_last_row {
+            self.s(0) * self.s(1) * binary_not(self.s(2))
+        } else {
+            self.s(0) * self.s(1) * binary_not(self.s_next(2))
+        }
     }
 }
