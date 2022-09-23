3f1549dab799e1ef8a79b40d1de6ea5a0f3b5bf1
Anjan Roy <hello@itzmeanjan.in>
2022-09-23 16:49:49 +0400

    add: modular division for element `e` ∈ GF(p^5) | p = 2^64 - 2^32 + 1
    - Doesn't panic even when divisor is zero element
    - Cost is 561 -cycles ( inversion + multiplication )
    
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- miden/tests/integration/stdlib/math/gfp5.rs
+++ miden/tests/integration/stdlib/math/gfp5.rs
@@ -1,5 +1,5 @@
 use super::{build_test, Felt};
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Div, Mul, Sub};
 use vm_core::StarkField;
 
 #[derive(Copy, Clone, Debug)]
@@ -144,6 +144,14 @@ impl Mul for GFp5 {
                 + self.a3 * rhs.a1
                 + self.a4 * rhs.a0,
         }
+    }
+}
+
+impl Div for GFp5 {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        self * rhs.inv()
     }
 }
 
@@ -318,4 +326,41 @@ fn test_gfp5_inv() {
     assert_eq!(strace[2], b.a2);
     assert_eq!(strace[3], b.a3);
     assert_eq!(strace[4], b.a4);
+}
+
+#[test]
+fn test_gfp5_div() {
+    let source = "
+    use.std::math::gfp5
+
+    begin
+        exec.gfp5::div
+    end";
+
+    let a = GFp5::rand();
+    let b = GFp5::rand();
+    let c = a / b;
+
+    let mut stack = [
+        a.a0.as_int(),
+        a.a1.as_int(),
+        a.a2.as_int(),
+        a.a3.as_int(),
+        a.a4.as_int(),
+        b.a0.as_int(),
+        b.a1.as_int(),
+        b.a2.as_int(),
+        b.a3.as_int(),
+        b.a4.as_int(),
+    ];
+    stack.reverse();
+
+    let test = build_test!(source, &stack);
+    let strace = test.get_last_stack_state();
+
+    assert_eq!(strace[0], c.a0);
+    assert_eq!(strace[1], c.a1);
+    assert_eq!(strace[2], c.a2);
+    assert_eq!(strace[3], c.a3);
+    assert_eq!(strace[4], c.a4);
 }
