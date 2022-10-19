8403219eb8bc74895e29dbdcfeb9f596f86a4c1a
Victor Lopez <victor@codx.io>
2022-10-19 15:58:27 +0200

    chore(ci): add release to tests
    This commit introduces the --release flag for the tests. It will
    sacrifice some build time to greatly improve the tests execution.
    
    One concern to add `release` is that `debug_assert` checks will no
    longer be performed. These are used in `miden` and upstream
    dependencies. To fix that, the `debug-assertions` compiler flag was
    added so these assertions will be executed.
    
    To ensure the tests always executes the debug assertions, a test
    `debug_assert_is_checked` was added. It will expect a panic and
    will fail by default.If it is set, then it will panic only if
    `debug_assert` is evaluated.

--- core/src/utils/mod.rs
+++ core/src/utils/mod.rs
@@ -100,3 +100,16 @@ pub fn new_array_vec<T: Debug, const N: usize>(capacity: usize) -> [Vec<T>; N] {
         .try_into()
         .expect("failed to convert vector to array")
 }
+
+#[test]
+#[should_panic]
+fn debug_assert_is_checked() {
+    // enforce the release checks to always have `RUSTFLAGS="-C debug-assertions".
+    //
+    // some upstream tests are performed with `debug_assert`, and we want to assert its correctness
+    // downstream.
+    //
+    // for reference, check
+    // https://github.com/maticnetwork/miden/issues/433
+    debug_assert!(false);
+}
