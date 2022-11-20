ff581b777d734fa4efb4375f0a0e2a8e520a8484
tohrnii <100405913+tohrnii@users.noreply.github.com>
2022-11-20 23:51:43 +0000

    fix: exclude decorators while adding asmop

--- assembly/src/parsers/mod.rs
+++ assembly/src/parsers/mod.rs
@@ -34,7 +34,7 @@ fn parse_op_token(
     let dec_len = decorators.len();
     // if assembler is in debug mode, populate decorators list with debug related
     // decorators like AsmOp.
-    if in_debug_mode {
+    if in_debug_mode && !is_decorator(op) {
         decorators.push((
             span_ops.len(),
             Decorator::AsmOp(AssemblyOp::new(op.to_string(), 1)),
@@ -202,7 +202,7 @@ fn parse_op_token(
         _ => return Err(AssemblyError::invalid_op(op)),
     }?;
 
-    if in_debug_mode {
+    if in_debug_mode && !is_decorator(op)  {
         let op_start = decorators[dec_len].0;
         // edit the number of cycles corresponding to the asmop decorator at an index
         if let Decorator::AsmOp(assembly_op) = &mut decorators[dec_len].1 {
@@ -211,6 +211,11 @@ fn parse_op_token(
     }
 
     Ok(())
+}
+
+/// Checks if an assembly instruction is a decorator.
+fn is_decorator(op: &Token) -> bool {
+    matches!(op.parts()[0], "adv")
 }
 
 // HELPER FUNCTIONS
