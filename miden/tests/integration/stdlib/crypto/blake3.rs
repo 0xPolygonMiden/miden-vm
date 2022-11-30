c564d22c172147895fb6311267d227548eec7823
Anjan Roy <hello@itzmeanjan.in>
2022-11-30 12:49:45 +0400

    chg: easier to read Blake3 test case
    Following suggestion https://github.com/maticnetwork/miden/pull/563#discussion_r1033000322
    
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- miden/tests/integration/stdlib/crypto/blake3.rs
+++ miden/tests/integration/stdlib/crypto/blake3.rs
@@ -1,6 +1,5 @@
-use super::{build_test, Felt, STACK_TOP_SIZE};
-use std::convert::TryInto;
-use vm_core::utils::IntoBytes;
+use super::{build_test, Felt};
+use vm_core::utils::{group_slice_elements, IntoBytes};
 
 #[test]
 fn blake3_hash_64_bytes() {
@@ -12,45 +11,28 @@ fn blake3_hash_64_bytes() {
     end
     ";
 
-    // prepare random input byte array
-    let i_digest_0: [u8; 32] = rand_utils::rand_array::<Felt, 4>().into_bytes();
-    let i_digest_1: [u8; 32] = rand_utils::rand_array::<Felt, 4>().into_bytes();
+    let input0 = rand_utils::rand_array::<Felt, 4>().into_bytes();
+    let input1 = rand_utils::rand_array::<Felt, 4>().into_bytes();
 
-    let mut i_digest = [0u8; 64];
-    i_digest[..32].copy_from_slice(&i_digest_0);
-    i_digest[32..].copy_from_slice(&i_digest_1);
+    let mut ibytes = [0u8; 64];
+    ibytes[..32].copy_from_slice(&input0);
+    ibytes[32..].copy_from_slice(&input1);
 
-    // allocate space on stack so that bytes can be converted to blake3 words
-    let mut i_words = [0u64; STACK_TOP_SIZE];
+    let ifelts = group_slice_elements::<u8, 4>(&ibytes)
+        .iter()
+        .map(|&bytes| u32::from_le_bytes(bytes) as u64)
+        .rev()
+        .collect::<Vec<u64>>();
 
-    // convert each of four consecutive little endian bytes (of input) to blake3 words
-    for (i, word) in i_words.iter_mut().enumerate().take(STACK_TOP_SIZE) {
-        let frm = i << 2;
-        let to = (i + 1) << 2;
-        *word = u32::from_le_bytes(i_digest[frm..to].try_into().unwrap()) as u64;
-    }
-    i_words.reverse();
+    let hasher = blake3::hash(&ibytes);
+    let obytes = hasher.as_bytes();
+    let ofelts = group_slice_elements::<u8, 4>(obytes)
+        .iter()
+        .map(|&bytes| u32::from_le_bytes(bytes) as u64)
+        .collect::<Vec<u64>>();
 
-    // use blake3 crate to compute 2-to-1 digest of byte array
-    let digest = blake3::hash(&i_digest);
-
-    // prepare digest in desired blake3 word form so that assertion writing becomes easier
-    let digest_bytes = digest.as_bytes();
-    let mut digest_words = [0u64; STACK_TOP_SIZE >> 1];
-
-    // convert each of four consecutive little endian bytes (of digest) to blake3 words
-    for (i, word) in digest_words
-        .iter_mut()
-        .enumerate()
-        .take(STACK_TOP_SIZE >> 1)
-    {
-        let frm = i << 2;
-        let to = (i + 1) << 2;
-        *word = u32::from_le_bytes(digest_bytes[frm..to].try_into().unwrap()) as u64;
-    }
-
-    let test = build_test!(source, &i_words);
-    test.expect_stack(&digest_words);
+    let test = build_test!(source, &ifelts);
+    test.expect_stack(&ofelts);
 }
 
 #[test]
@@ -63,41 +45,20 @@ fn blake3_hash_32_bytes() {
     end
     ";
 
-    // prepare random input byte array
-    let input = rand_utils::rand_array::<Felt, 4>().into_bytes();
+    let ibytes = rand_utils::rand_array::<Felt, 4>().into_bytes();
+    let ifelts = group_slice_elements::<u8, 4>(&ibytes)
+        .iter()
+        .map(|&bytes| u32::from_le_bytes(bytes) as u64)
+        .rev()
+        .collect::<Vec<u64>>();
 
-    let mut i_digest = [0u8; 32];
-    i_digest.copy_from_slice(&input);
+    let hasher = blake3::hash(&ibytes);
+    let obytes = hasher.as_bytes();
+    let ofelts = group_slice_elements::<u8, 4>(obytes)
+        .iter()
+        .map(|&bytes| u32::from_le_bytes(bytes) as u64)
+        .collect::<Vec<u64>>();
 
-    // allocate space on stack so that bytes can be converted to blake3 words
-    let mut i_words = [0u64; STACK_TOP_SIZE >> 1];
-
-    // convert each of four consecutive little endian bytes (of input) to blake3 words
-    for (i, word) in i_words.iter_mut().enumerate().take(STACK_TOP_SIZE >> 1) {
-        let frm = i << 2;
-        let to = (i + 1) << 2;
-        *word = u32::from_le_bytes(i_digest[frm..to].try_into().unwrap()) as u64;
-    }
-    i_words.reverse();
-
-    // use blake3 crate to compute digest of 32 bytes
-    let digest = blake3::hash(&i_digest);
-
-    // prepare digest in desired blake3 word form so that assertion writing becomes easier
-    let digest_bytes = digest.as_bytes();
-    let mut digest_words = [0u64; STACK_TOP_SIZE >> 1];
-
-    // convert each of four consecutive little endian bytes (of digest) to blake3 words
-    for (i, word) in digest_words
-        .iter_mut()
-        .enumerate()
-        .take(STACK_TOP_SIZE >> 1)
-    {
-        let frm = i << 2;
-        let to = (i + 1) << 2;
-        *word = u32::from_le_bytes(digest_bytes[frm..to].try_into().unwrap()) as u64;
-    }
-
-    let test = build_test!(source, &i_words);
-    test.expect_stack(&digest_words);
+    let test = build_test!(source, &ifelts);
+    test.expect_stack(&ofelts);
 }
