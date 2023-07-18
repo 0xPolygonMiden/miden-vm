09186fd5298b93b98d3ab66a0c4eb6a005049ab8
Bobbin Threadbare <bobbinth@protonmail.com>
2023-07-17 20:38:12 -0700

    fix: typos in bundle command

--- miden/src/cli/bundle.rs
+++ miden/src/cli/bundle.rs
@@ -8,13 +8,13 @@ use structopt::StructOpt;
     about = "Bundles .masm files into a single .masl library"
 )]
 pub struct BundleCmd {
-    /// Path to a directory containg the `.masm` files which are part of the library
+    /// Path to a directory containing the `.masm` files which are part of the library.
     #[structopt(parse(from_os_str))]
     dir: PathBuf,
-    /// Defines the top-level namespace, e.g. `mylib`, oherwise the directory name is used.
+    /// Defines the top-level namespace, e.g. `mylib`, otherwise the directory name is used.
     #[structopt(short, long)]
     namespace: Option<String>,
-    /// Version of you library, defaults to `0.1.0`.
+    /// Version of the library, defaults to `0.1.0`.
     #[structopt(short, long, default_value = "0.1.0")]
     version: String,
 }
@@ -22,7 +22,7 @@ pub struct BundleCmd {
 impl BundleCmd {
     pub fn execute(&self) -> Result<(), String> {
         println!("============================================================");
-        println!("Compile library");
+        println!("Build library");
         println!("============================================================");
 
         let namespace = match &self.namespace {
@@ -38,15 +38,19 @@ impl BundleCmd {
         let library_namespace =
             LibraryNamespace::try_from(namespace.clone()).expect("invalid base namespace");
         let version = Version::try_from(self.version.as_ref()).expect("invalid cargo version");
-        let locations = true; // store & load locations by default
-        let stdlib =
-            MaslLibrary::read_from_dir(self.dir.clone(), library_namespace, locations, version)
-                .map_err(|e| e.to_string())?;
+        let with_source_locations = true;
+        let stdlib = MaslLibrary::read_from_dir(
+            self.dir.clone(),
+            library_namespace,
+            with_source_locations,
+            version,
+        )
+        .map_err(|e| e.to_string())?;
 
         // write the masl output
         stdlib.write_to_dir(self.dir.clone()).map_err(|e| e.to_string())?;
 
-        println!("Compiled library {}", namespace);
+        println!("Built library {}", namespace);
 
         Ok(())
     }
