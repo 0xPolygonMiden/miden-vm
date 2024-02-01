9758c7a929749191699f5cc15adcdc2a95294f9c
Scott Dieringer <46901491+scottdieringer@users.noreply.github.com>
2024-02-01 10:27:33 -0600

    tests: Have the powers of tau test working.  Need to refactor the test and then start adding tests for the falcon DSA decorators.

--- stdlib/tests/crypto/falcon.rs
+++ stdlib/tests/crypto/falcon.rs
@@ -12,16 +12,6 @@ const Q: u64 = (M - 1) / 2;
 const J: u64 = 512 * M * M;
 
 #[test]
-fn test_falcon_verify() {
-    let keypair = KeyPair::new().unwrap();
-
-    let message = rand_vector::<Felt>(4).try_into().unwrap();
-
-    let test = generate_test_verify(keypair, message);
-    test.expect_stack(&[])
-}
-
-#[test]
 fn test_falcon_norm_sq() {
     let source = "
     use.std::crypto::dsa::rpo_falcon512
@@ -67,59 +57,52 @@ fn test_falcon_diff_mod_q() {
 
 #[test]
 fn test_falcon_powers_of_tau() {
-    // let source = "
-    // use.std::crypto::dsa::rpo_falcon512
-    //
-    // begin
-    //     exec.rpo_falcon512::powers_of_tau
-    // end
-    // ";
-
     let source = "
     use.std::crypto::dsa::rpo_falcon512
 
     begin
-        push.1 push.0.0.0
-        dup.6 add.1 swap.7
-        mem_storew
-        drop drop
-
-        repeat.2
-            dupw ext2mul
-
-            movup.3 movup.3
-
-            dup.6 add.1 swap.7 mem_storew
-
-            drop drop
-        end
-
-        dropw
+        exec.rpo_falcon512::powers_of_tau
     end
     ";
 
     let tau = rand_value::<QuadFelt>();
-    let tau_squared = tau.square();
+    let tau_ptr = 0_u32;
+    let (tau_0, tau_1) = ext_element_to_ints(tau);
 
+    let mut tau_power: QuadFelt;
+    let mut elem_0: u64;
+    let mut elem_1: u64;
+    let mut expected_memory = vec![0; 513 * 4];
+    expected_memory[0] = 1;
 
+    for i in (1..513usize) {
+        tau_power = tau.exp(i as u64);
+        (elem_0, elem_1) = ext_element_to_ints(tau_power);
+        expected_memory[i * 4] = elem_0;
+        expected_memory[i * 4 + 1] = elem_1;
+        expected_memory[i * 4 + 2] = expected_memory[i * 4 - 4];
+        expected_memory[i * 4 + 3] = expected_memory[i * 4 - 3];
+    }
 
-    let (elem_0, elem_1) = ext_element_to_ints(tau);
-    let (elem_2, elem_3) = ext_element_to_ints(tau_squared);
-    println!("elem_0 is: {:?}", elem_0);
-    println!("elem_1 is: {:?}", elem_1);
-
-    println!("elem_2 is: {:?}", elem_2);
-    println!("elem_3 is: {:?}", elem_3);
-
-
-    let stack_init = [6, elem_0, elem_1];
+    let stack_init = [tau_ptr.into(), tau_0, tau_1];
 
     let test = build_test!(source, &stack_init);
 
-    let expected_stack = &[9];
+    let expected_stack = &[<u32 as Into<u64>>::into(tau_ptr) + 513];
 
-    test.expect_stack_and_memory(expected_stack, 6, &[1, 0, 0, 0, elem_0, elem_1, 1, 0, elem_2, elem_3, elem_1, elem_0]);
+    test.expect_stack_and_memory(expected_stack, tau_ptr, &expected_memory);
 }
+
+#[test]
+fn test_falcon_verify() {
+    let keypair = KeyPair::new().unwrap();
+
+    let message = rand_vector::<Felt>(4).try_into().unwrap();
+
+    let test = generate_test_verify(keypair, message);
+    test.expect_stack(&[])
+}
+
 
 fn generate_test_verify(keypair: KeyPair, message: Word) -> Test {
     let source = "
