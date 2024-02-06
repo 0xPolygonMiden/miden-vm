ed978fca6a61c74209f9f037bbf133c2b2017f31
Philippe Laferriere <plafer@protonmail.com>
2024-02-06 14:58:14 -0500

    Re-export `SmtProof{Error}`

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -13,7 +13,8 @@ pub mod crypto {
         pub use miden_crypto::merkle::{
             DefaultMerkleStore, EmptySubtreeRoots, InnerNodeInfo, LeafIndex, MerkleError,
             MerklePath, MerkleStore, MerkleTree, Mmr, MmrPeaks, NodeIndex, PartialMerkleTree,
-            RecordingMerkleStore, SimpleSmt, Smt, StoreNode, TieredSmt, SMT_DEPTH,
+            RecordingMerkleStore, SimpleSmt, Smt, SmtProof, SmtProofError, StoreNode, TieredSmt,
+            SMT_DEPTH,
         };
     }
 
