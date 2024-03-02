4a04bc6b90534cd1e912eeaa224de48e4d35c50c
Paul Schoenfelder <955793+bitwalker@users.noreply.github.com>
2024-03-02 15:54:03 -0500

    feat: add assert_matches macro to core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -6,6 +6,49 @@ extern crate std;
 #[macro_use]
 extern crate alloc;
 
+/// This is an implementation of `std::assert_matches::assert_matches`
+/// so it can be removed when that feature stabilizes upstream
+#[macro_export]
+macro_rules! assert_matches {
+    ($left:expr, $(|)? $( $pattern:pat_param )|+ $( if $guard: expr )? $(,)?) => {
+        match $left {
+            $( $pattern )|+ $( if $guard )? => {}
+            ref left_val => {
+                panic!(r#"
+assertion failed: `(left matches right)`
+    left: `{:?}`,
+    right: `{}`"#, left_val, stringify!($($pattern)|+ $(if $guard)?));
+            }
+        }
+    };
+
+    ($left:expr, $(|)? $( $pattern:pat_param )|+ $( if $guard: expr )?, $msg:literal $(,)?) => {
+        match $left {
+            $( $pattern )|+ $( if $guard )? => {}
+            ref left_val => {
+                panic!(concat!(r#"
+assertion failed: `(left matches right)`
+    left: `{:?}`,
+    right: `{}`
+"#, $msg), left_val, stringify!($($pattern)|+ $(if $guard)?));
+            }
+        }
+    };
+
+    ($left:expr, $(|)? $( $pattern:pat_param )|+ $( if $guard: expr )?, $msg:literal, $($arg:tt)+) => {
+        match $left {
+            $( $pattern )|+ $( if $guard )? => {}
+            ref left_val => {
+                panic!(concat!(r#"
+assertion failed: `(left matches right)`
+    left: `{:?}`,
+    right: `{}`
+"#, $msg), left_val, stringify!($($pattern)|+ $(if $guard)?), $($arg)+);
+            }
+        }
+    }
+}
+
 pub mod chiplets;
 pub mod errors;
 
