9227303de6faba236893eb6110f29148d3dd2752
Paul Schoenfelder <955793+bitwalker@users.noreply.github.com>
2024-03-02 17:17:25 -0500

    wip(2/n): refactor invocation target type
    This commit builds on changes to the procedure types to represent a
    richer set of targets, with varying degrees of specificity:
    
    * The `MastRoot` variant remains unchanged, but gains a source span for
      use later during compilation
    * The `ProcedureName` variant represents a local name
    * The `ProcedurePath` variant represents an unresolved projection of an
      imported module/function. It depends on the current module context to
      resolve.
    * The `AbsoluteProcedurePath` variant represents a resolved projection
      of an imported module/function. This type is used when we have
      resolved a `ProcedurePath` to an imported module, and thus know the
      absolute path of the imported function. However, the distinction
      between an absolute path which is "fully-resolved", i.e. not an alias,
      and an absolute path which is "partially-resolved", i.e.
      possibly-aliased, is not represented here. Instead, that distinction
      is implicit dependiing on the phase of compilation we are in. This
      will become clearer in later commits.
    
    Later, this type will be used to represent the targets of any
    instruction which references a callable (name or mast root)

--- assembly/src/ast/invocation_target.rs
+++ assembly/src/ast/invocation_target.rs
@@ -1,67 +1,143 @@
-use super::{
-    parsers::decode_hex_rpo_digest_label, LibraryPath, ParsingError, ProcedureName, RpoDigest,
-    Token,
+use crate::{
+    ast::{AstSerdeOptions, Ident, ProcedureName},
+    ByteReader, ByteWriter, Deserializable, DeserializationError, LibraryPath, RpoDigest,
+    Serializable, SourceSpan, Span, Spanned,
 };
 
-/// Describes targets of `exec`, `call`, and `syscall` instructions.
-pub enum InvocationTarget<'a> {
-    MastRoot(RpoDigest),
-    ProcedureName(ProcedureName),
-    ProcedurePath {
-        name: ProcedureName,
-        module: &'a str,
-    },
+/// Represents the kind of an invocation
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(u8)]
+pub enum InvokeKind {
+    Exec = 0,
+    Call,
+    SysCall,
 }
 
-impl<'a> InvocationTarget<'a> {
-    /// Parses the provided label into an invocation target.
-    ///
-    /// A label of an invoked procedure must comply with the following rules:
-    /// - It can be a hexadecimal string representing a MAST root digest ([RpoDigest]). In this case,
-    ///   the label must start with "0x" and must be followed by a valid hexadecimal string
-    ///   representation of an [RpoDigest].
-    /// - It can contain a single procedure name. In this case, the label must comply with procedure
-    ///   name rules.
-    /// - It can contain module name followed by procedure name (e.g., "module::procedure"). In this
-    ///   case both module and procedure name must comply with relevant name rules.
-    ///
-    /// All other combinations will result in an error.
-    pub fn parse(label: &'a str, token: &'a Token) -> Result<Self, ParsingError> {
-        if label.starts_with("0x") {
-            return Ok(InvocationTarget::MastRoot(
-                decode_hex_rpo_digest_label(label)
-                    .map_err(|err| ParsingError::invalid_proc_root_invocation(token, label, err))?,
-            ));
-        }
+/// Represents a specific invocation
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Invoke {
+    pub kind: InvokeKind,
+    pub target: InvocationTarget,
+}
+impl Spanned for Invoke {
+    fn span(&self) -> SourceSpan {
+        self.target.span()
+    }
+}
+impl Invoke {
+    pub fn new(kind: InvokeKind, target: InvocationTarget) -> Self {
+        Self { kind, target }
+    }
+}
 
-        let num_components = LibraryPath::validate(label)
-            .map_err(|_| ParsingError::invalid_proc_invocation(token, label))?;
-
-        match num_components {
-            1 => {
-                let name = Self::parse_proc_name(label, token)?;
-                Ok(InvocationTarget::ProcedureName(name))
+/// Describes targets of `exec`, `call`, and `syscall` instructions.
+///
+/// A label of an invoked procedure must comply with the following rules:
+/// - It can be a hexadecimal string representing a MAST root digest ([RpoDigest]). In this case,
+///   the label must start with "0x" and must be followed by a valid hexadecimal string
+///   representation of an [RpoDigest].
+/// - It can contain a single procedure name. In this case, the label must comply with procedure
+///   name rules.
+/// - It can contain module name followed by procedure name (e.g., "module::procedure"). In this
+///   case both module and procedure name must comply with relevant name rules.
+///
+/// All other combinations will result in an error.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(u8)]
+pub enum InvocationTarget {
+    /// An absolute procedure reference, but opaque in that
+    /// we do not know where the callee is defined. However,
+    /// it does not actually matter, we consider such references
+    /// to be _a priori_ valid.
+    MastRoot(Span<RpoDigest>) = 0,
+    /// A locally-defined procedure
+    ProcedureName(ProcedureName) = 1,
+    /// A context-sensitive procedure path, which
+    /// references the name of an import in the containing
+    /// module.
+    ProcedurePath { name: ProcedureName, module: Ident } = 2,
+    /// A fully-resolved procedure path, which refers
+    /// to a specific externally-defined procedure
+    /// with its full path
+    AbsoluteProcedurePath {
+        name: ProcedureName,
+        path: LibraryPath,
+    } = 3,
+}
+impl Spanned for InvocationTarget {
+    fn span(&self) -> SourceSpan {
+        match self {
+            Self::MastRoot(ref spanned) => spanned.span(),
+            Self::ProcedureName(ref spanned) => spanned.span(),
+            Self::ProcedurePath { ref name, .. } | Self::AbsoluteProcedurePath { ref name, .. } => {
+                name.span()
             }
-            2 => {
-                let parts = label.split_once(LibraryPath::PATH_DELIM).expect("no components");
-                let name = Self::parse_proc_name(parts.1, token)?;
-                Ok(InvocationTarget::ProcedurePath {
-                    name,
-                    module: parts.0,
-                })
-            }
-            _ => Err(ParsingError::invalid_proc_invocation(token, label)),
         }
     }
-
-    // HELPER FUNCTIONS
-    // --------------------------------------------------------------------------------------------
-
-    /// Attempts to interpret a label as a procedure name
-    fn parse_proc_name(label: &'a str, token: &'a Token) -> Result<ProcedureName, ParsingError> {
-        match ProcedureName::try_from(label) {
-            Ok(name) => Ok(name),
-            Err(err) => Err(ParsingError::invalid_proc_name(token, err)),
+}
+impl InvocationTarget {
+    fn tag(&self) -> u8 {
+        // SAFETY: This is safe because we have given this enum a
+        // primitive representation with #[repr(u8)], with the first
+        // field of the underlying union-of-structs the discriminant
+        unsafe { *<*const _>::from(self).cast::<u8>() }
+    }
+}
+impl Serializable for InvocationTarget {
+    fn write_into<W: ByteWriter>(&self, target: &mut W) {
+        target.write_u8(self.tag());
+        match self {
+            Self::MastRoot(spanned) => {
+                spanned.write_into(target, AstSerdeOptions::new(false, true))
+            }
+            Self::ProcedureName(name) => {
+                name.write_into_with_options(target, AstSerdeOptions::new(false, true))
+            }
+            Self::ProcedurePath { name, module } => {
+                name.write_into_with_options(target, AstSerdeOptions::new(false, true));
+                module.write_into(target);
+            }
+            Self::AbsoluteProcedurePath { name, path } => {
+                name.write_into_with_options(target, AstSerdeOptions::new(false, true));
+                path.write_into(target);
+            }
+        }
+    }
+}
+impl Deserializable for InvocationTarget {
+    fn read_from<R: ByteReader>(source: &mut R) -> Result<Self, DeserializationError> {
+        match source.read_u8()? {
+            0 => {
+                let root = Span::<RpoDigest>::read_from(source, AstSerdeOptions::new(false, true))?;
+                Ok(Self::MastRoot(root))
+            }
+            1 => {
+                let name = ProcedureName::read_from_with_options(
+                    source,
+                    AstSerdeOptions::new(false, true),
+                )?;
+                Ok(Self::ProcedureName(name))
+            }
+            2 => {
+                let name = ProcedureName::read_from_with_options(
+                    source,
+                    AstSerdeOptions::new(false, true),
+                )?;
+                let module = Ident::read_from(source)?;
+                Ok(Self::ProcedurePath { name, module })
+            }
+            3 => {
+                let name = ProcedureName::read_from_with_options(
+                    source,
+                    AstSerdeOptions::new(false, true),
+                )?;
+                let path = LibraryPath::read_from(source)?;
+                Ok(Self::AbsoluteProcedurePath { name, path })
+            }
+            n => Err(DeserializationError::InvalidValue(format!(
+                "{} is not a valid invocation target type",
+                n
+            ))),
         }
     }
 }
