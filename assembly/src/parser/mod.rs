e5e2712ddd832782d3a21ffcae1cef733247b1a5
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2024-03-15 20:57:50 -0400

    docs: document some undocumented traits/types

--- assembly/src/parser/mod.rs
+++ assembly/src/parser/mod.rs
@@ -40,12 +40,38 @@ type ParseError<'a> = lalrpop_util::ParseError<u32, Token<'a>, ParsingError>;
 // MODULE PARSER
 // ================================================================================================
 
+/// This is a wrapper around the lower-level parser infrastructure which handles orchestrating all
+/// of the pieces needed to parse a [Module] from source, and run semantic analysis on it.
+///
+/// In the vast majority of cases though, you will want to use the more ergonomic [Module::parse_str]
+/// or [Module::parse_file] APIs instead.
 #[derive(Default)]
 pub struct ModuleParser {
+    /// The kind of module we're parsing.
+    ///
+    /// This is used when performing semantic analysis to detect when various invalid constructions
+    /// are encountered, such as use of the `syscall` instruction in a kernel module.
     kind: ast::ModuleKind,
+    /// A set of interned strings allocated during parsing/semantic analysis.
+    ///
+    /// This is a very primitive and imprecise way of interning strings, but was the least invasive
+    /// at the time the new parser was implemented. In essence, we avoid duplicating allocations for
+    /// frequently occuring strings, by tracking which strings we've seen before, and sharing a reference
+    /// counted pointer instead.
+    ///
+    /// We may want to replace this eventually with a proper interner, so that we can also gain the
+    /// benefits commonly provided by interned string handles (e.g. cheap equality comparisons, no ref-
+    /// counting overhead, copyable and of smaller size).
+    ///
+    /// Note that [Ident], [ProcedureName], [LibraryPath] and others are all implemented in terms of
+    /// either the actual reference-counted string, e.g. `Arc<str>`, or in terms of [Ident], which is
+    /// essentially the former wrapped in a [SourceSpan]. If we ever replace this with a better interner,
+    /// we will also want to update those types to be in terms of whatever the handle type of the interner
+    /// is.
     interned: BTreeSet<Arc<str>>,
 }
 impl ModuleParser {
+    /// Construct a new parser for the given `kind` of [Module]
     pub fn new(kind: ast::ModuleKind) -> Self {
         Self {
             kind,
@@ -53,6 +79,7 @@ impl ModuleParser {
         }
     }
 
+    /// Parse a [Module] from `source`, and give it the provided `path`.
     pub fn parse(
         &mut self,
         path: LibraryPath,
@@ -64,12 +91,19 @@ impl ModuleParser {
     }
 }
 
+/// This is used in tests to parse `source` as a set of raw [ast::Form]s rather than as a [Module].
+///
+/// NOTE: This does _not_ run semantic analysis.
 #[cfg(any(test, feature = "testing"))]
 pub fn parse_forms(source: Arc<SourceFile>) -> Result<Vec<ast::Form>, ParsingError> {
     let mut interned = BTreeSet::default();
     parse_forms_internal(source, &mut interned)
 }
 
+/// Parse `source` as a set of [ast::Form]s
+///
+/// Aside from catching syntax errors, this does little validation of the resulting forms, that is
+/// handled by semantic analysis, which the caller is expected to perform next.
 fn parse_forms_internal(
     source: Arc<SourceFile>,
     interned: &mut BTreeSet<Arc<str>>,
