4678012abf9bbe572ec18305d1958951289bc277
Philippe Laferriere <plafer@protonmail.com>
2024-07-05 14:28:27 -0400

    put Lagrange kernel column at the end

--- air/src/trace/logup.rs
+++ air/src/trace/logup.rs
@@ -1,7 +1,7 @@
 use super::GKR_LOGUP_AUX_TRACE_OFFSET;
 
-/// A column that holds the Lagrange kernel.
-pub const LAGRANGE_KERNEL_COL_IDX: usize = GKR_LOGUP_AUX_TRACE_OFFSET;
-
 /// The "s" column as described in the LogUp-GKR documentation.
-pub const S_COL_IDX: usize = GKR_LOGUP_AUX_TRACE_OFFSET + 1;
+pub const S_COL_IDX: usize = GKR_LOGUP_AUX_TRACE_OFFSET;
+
+/// A column that holds the Lagrange kernel.
+pub const LAGRANGE_KERNEL_COL_IDX: usize = GKR_LOGUP_AUX_TRACE_OFFSET + 1;
