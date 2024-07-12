781fc7376b8f557b2123d1233ca14a9f914cfb4a
Philippe Laferriere <plafer@protonmail.com>
2024-07-12 15:46:10 -0400

    fix asserts

--- core/src/mast/serialization/info.rs
+++ core/src/mast/serialization/info.rs
@@ -246,12 +246,12 @@ impl MastNodeType {
     /// - Panics if either `left_value` or `right_value` doesn't fit in 30 bits.
     fn encode_u32_pair(left_value: u32, right_value: u32) -> u64 {
         assert!(
-            left_value.leading_zeros() < 2,
+            left_value.leading_zeros() >= 2,
             "MastNodeType::encode_u32_pair: left value doesn't fit in 30 bits: {}",
             left_value
         );
         assert!(
-            right_value.leading_zeros() < 2,
+            right_value.leading_zeros() >= 2,
             "MastNodeType::encode_u32_pair: right value doesn't fit in 30 bits: {}",
             right_value
         );
