269caf9fd559b8774ac3effab13c7da057c1a024
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2024-07-18 13:01:41 +0200

    Add success message

--- scripts/check-changelog.sh
+++ scripts/check-changelog.sh
@@ -11,9 +11,11 @@ else
     # a changelog check is required
     # fail if the diff is empty
     if git diff --exit-code "origin/${BASE_REF}" -- "${CHANGELOG_FILE}"; then
-        >&2 echo "Changes should come with an entry in the CHANGELOG.md file. This behavior
+        >&2 echo "Changes should come with an entry in the \"CHANGELOG.md\" file. This behavior
 can be overridden by using the \"no changelog\" label, which is used for changes
 that are trivial / explicitely stated not to require a changelog entry."
         exit 1
     fi
+
+    echo "The \"CHANGELOG.md\" file has been updated."
 fi
