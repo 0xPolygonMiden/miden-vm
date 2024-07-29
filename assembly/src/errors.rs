ede435ae93c102fb418f02e78738766a2194a039
Bobbin Threadbare <bobbinth@protonmail.com>
2024-07-28 23:57:00 -0700

    chore: remove unused assembly errors

--- assembly/src/errors.rs
+++ assembly/src/errors.rs
@@ -2,10 +2,13 @@ use alloc::{string::String, sync::Arc, vec::Vec};
 use vm_core::mast::MastForestError;
 
 use crate::{
-    ast::{FullyQualifiedProcedureName, ProcedureName},
+    ast::FullyQualifiedProcedureName,
     diagnostics::{Diagnostic, RelatedError, RelatedLabel, Report, SourceFile},
-    KernelError, LibraryNamespace, LibraryPath, RpoDigest, SourceSpan,
+    LibraryNamespace, LibraryPath, RpoDigest, SourceSpan,
 };
+
+// ASSEMBLY ERROR
+// ================================================================================================
 
 /// An error which can be generated while compiling a Miden assembly program into a MAST.
 #[derive(Debug, thiserror::Error, Diagnostic)]
@@ -24,20 +27,11 @@ pub enum AssemblyError {
     #[error("found a cycle in the call graph, involving these procedures: {}", nodes.as_slice().join(", "))]
     #[diagnostic()]
     Cycle { nodes: Vec<String> },
-    #[error("attempted to provide multiple kernel modules to the assembler")]
-    #[diagnostic()]
-    ConflictingKernels,
     #[error("two procedures found with same mast root, but conflicting definitions ('{first}' and '{second}')")]
     #[diagnostic()]
     ConflictingDefinitions {
         first: FullyQualifiedProcedureName,
         second: FullyQualifiedProcedureName,
-    },
-    #[error("conflicting entrypoints were provided (in '{first}' and '{second}')")]
-    #[diagnostic()]
-    MultipleEntry {
-        first: LibraryPath,
-        second: LibraryPath,
     },
     #[error("duplicate definition found for module '{path}'")]
     #[diagnostic()]
@@ -51,32 +45,11 @@ pub enum AssemblyError {
         source_file: Option<Arc<SourceFile>>,
         path: LibraryPath,
     },
-    #[error("undefined local procedure '{name}'")]
-    #[diagnostic()]
-    UndefinedLocalProcedure {
-        #[label]
-        name: ProcedureName,
-        #[source_code]
-        source_file: Option<Arc<SourceFile>>,
-    },
-    #[error("expected procedure with MAST root '{digest}' to be found in assembler cache, but it was not")]
-    #[diagnostic()]
-    UndefinedCallSetProcedure { digest: RpoDigest },
     #[error("module namespace is inconsistent with library ('{actual}' vs '{expected}')")]
     #[diagnostic()]
     InconsistentNamespace {
         expected: LibraryNamespace,
         actual: LibraryNamespace,
-    },
-    #[error(
-        "re-exported procedure '{name}' is self-recursive: resolving the alias is not possible"
-    )]
-    #[diagnostic()]
-    RecursiveAlias {
-        #[label("recursion starts here")]
-        name: FullyQualifiedProcedureName,
-        #[source_code]
-        source_file: Option<Arc<SourceFile>>,
     },
     #[error("invalid syscall: '{callee}' is not an exported kernel procedure")]
     #[diagnostic()]
@@ -106,24 +79,6 @@ pub enum AssemblyError {
         #[source_code]
         source_file: Option<Arc<SourceFile>>,
     },
-    #[error(transparent)]
-    #[diagnostic(transparent)]
-    Parsing(#[from] crate::parser::ParsingError),
-    #[error(transparent)]
-    #[diagnostic(transparent)]
-    SemanticAnalysis(#[from] crate::sema::SemanticAnalysisError),
-    #[error(transparent)]
-    #[diagnostic(transparent)]
-    Syntax(#[from] crate::sema::SyntaxError),
-    #[error(transparent)]
-    #[diagnostic()]
-    Kernel(#[from] KernelError),
-    #[error(transparent)]
-    #[diagnostic(transparent)]
-    Library(#[from] crate::library::LibraryError),
-    #[error(transparent)]
-    #[cfg(feature = "std")]
-    Io(#[from] std::io::Error),
     #[error(transparent)]
     #[diagnostic(transparent)]
     Other(#[from] RelatedError),
