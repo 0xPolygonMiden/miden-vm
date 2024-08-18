6353631607c348a9ff9b7e5851a13e27abe61213
sergerad <serge.radinovich@immutable.com>
2024-08-19 08:17:40 +1200

    move bench to miden crate

--- miden/benches/deserialize_std_lib.rs
+++ miden/benches/deserialize_std_lib.rs
@@ -2,17 +2,15 @@ use std::time::Duration;
 
 use assembly::{utils::Deserializable, Library};
 use criterion::{criterion_group, criterion_main, Criterion};
+use stdlib::StdLibrary;
 
 fn deserialize_std_lib(c: &mut Criterion) {
-    // Load the standard library from the compiled assets.
-    let bytes = include_bytes!(concat!(env!("OUT_DIR"), "/assets/std.masl"));
-
-    // Setup and run the benchmarks.
     let mut group = c.benchmark_group("deserialize_std_lib");
     group.measurement_time(Duration::from_secs(15));
     group.bench_function("read_from_bytes", |bench| {
         bench.iter(|| {
-            let _ = Library::read_from_bytes(bytes).expect("failed to read std masl!");
+            let _ =
+                Library::read_from_bytes(StdLibrary::SERIALIZED).expect("failed to read std masl!");
         });
     });
 
