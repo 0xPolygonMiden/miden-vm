8243fa2318baadd44c51fcbd16d07e461e8e24aa
Philippe Laferriere <plafer@protonmail.com>
2024-09-03 18:15:57 -0400

    fmt

--- assembly/src/assembler/instruction/procedures.rs
+++ assembly/src/assembler/instruction/procedures.rs
@@ -63,7 +63,8 @@ impl Assembler {
         let mast_root = {
             let proc_body_id =
                 self.resolve_target(InvokeKind::ProcRef, callee, proc_ctx, mast_forest_builder)?;
-            // Note: it's ok to `unwrap()` here since `proc_body_id` was returned from `mast_forest_builder`
+            // Note: it's ok to `unwrap()` here since `proc_body_id` was returned from
+            // `mast_forest_builder`
             mast_forest_builder.get_mast_node(proc_body_id).unwrap().digest()
         };
 
