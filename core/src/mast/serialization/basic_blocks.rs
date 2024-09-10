f3b5a45493a7a86c50886861763db0e0f97e433a
Philippe Laferriere <plafer@protonmail.com>
2024-09-10 08:33:42 -0400

    update docstring for `BasicBlockDataDecoder`

--- core/src/mast/serialization/basic_blocks.rs
+++ core/src/mast/serialization/basic_blocks.rs
@@ -66,7 +66,7 @@ impl<'a> BasicBlockDataDecoder<'a> {
     }
 }
 
-/// Mutators
+/// Decoding methods
 impl<'a> BasicBlockDataDecoder<'a> {
     pub fn decode_operations_and_decorators(
         &self,
