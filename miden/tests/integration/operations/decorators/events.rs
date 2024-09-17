a4a4925d00a11322dea8d3b181ffc00720dd5a95
Yason Khaburzaniya <yason@hey.com>
2024-09-17 11:50:01 -0700

    chore: add some integration tests for debugging

--- miden/tests/integration/operations/decorators/events.rs
+++ miden/tests/integration/operations/decorators/events.rs
@@ -1,5 +1,6 @@
 use assembly::Assembler;
 use processor::{ExecutionOptions, Program};
+use prover::StackInputs;
 
 use super::TestHost;
 
@@ -16,7 +17,8 @@ fn test_event_handling() {
     // compile and execute program
     let program: Program = Assembler::default().assemble_program(source).unwrap();
     let mut host = TestHost::default();
-    processor::execute(&program, Default::default(), &mut host, Default::default()).unwrap();
+    processor::execute(&program, StackInputs::default(), &mut host, ExecutionOptions::default())
+        .unwrap();
 
     // make sure events were handled correctly
     let expected = vec![1, 2];
@@ -38,18 +40,64 @@ fn test_trace_handling() {
     let mut host = TestHost::default();
 
     // execute program with disabled tracing
-    processor::execute(&program, Default::default(), &mut host, Default::default()).unwrap();
+    processor::execute(&program, StackInputs::default(), &mut host, ExecutionOptions::default())
+        .unwrap();
     let expected = Vec::<u32>::new();
     assert_eq!(host.trace_handler, expected);
 
     // execute program with enabled tracing
     processor::execute(
         &program,
-        Default::default(),
+        StackInputs::default(),
         &mut host,
         ExecutionOptions::default().with_tracing(),
     )
     .unwrap();
     let expected = vec![1, 2];
     assert_eq!(host.trace_handler, expected);
+}
+
+#[test]
+fn test_debug_with_debugging() {
+    let source: &str = "\
+    begin
+        push.1
+        debug.stack
+        debug.mem
+    end";
+
+    // compile and execute program
+    let program: Program = Assembler::default().assemble_program(source).unwrap();
+    let mut host = TestHost::default();
+    processor::execute(
+        &program,
+        StackInputs::default(),
+        &mut host,
+        ExecutionOptions::default().with_debugging(),
+    )
+    .unwrap();
+
+    // Expect to see the debug.stack and debug.mem commands
+    let expected = vec!["stack", "mem"];
+    assert_eq!(host.debug_handler, expected);
+}
+
+#[test]
+fn test_debug_without_debugging() {
+    let source: &str = "\
+    begin
+        push.1
+        debug.stack
+        debug.mem
+    end";
+
+    // compile and execute program
+    let program: Program = Assembler::default().assemble_program(source).unwrap();
+    let mut host = TestHost::default();
+    processor::execute(&program, StackInputs::default(), &mut host, ExecutionOptions::default())
+        .unwrap();
+
+    // Expect to see no debug commands
+    let expected: Vec<String> = vec![];
+    assert_eq!(host.debug_handler, expected);
 }
