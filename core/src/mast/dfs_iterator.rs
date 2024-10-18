cf7bac86519d87d6bd7802cffd21006ad4f7302d
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2024-10-18 09:04:39 +0200

    fix(core): Use `roots` of the forest for discovering trees

--- core/src/mast/dfs_iterator.rs
+++ core/src/mast/dfs_iterator.rs
@@ -4,25 +4,30 @@ use crate::mast::{MastForest, MastNode, MastNodeId};
 
 /// Depth First Search Iterator in Post Order for [`MastForest`]s.
 ///
-/// This iterator iterates through all nodes of a forest exactly once.
+/// This iterator iterates through all **reachable** nodes of a forest exactly once.
 ///
-/// Since a `MastForest` does not have a single entrypoint a DFS is a bit more involved.
+/// Since a `MastForest` has multiple possible entrypoints in the form of its roots, a depth-first
+/// search must visit all of those roots and the trees they form.
 ///
-/// We need a way to discover a tree of the forest. For instance, consider this `MastForest`:
+/// For instance, consider this `MastForest`:
 ///
 /// ```text
-/// [Join(1, 2), Block(foo), Block(bar), External(qux)]
+/// Nodes: [Join(1, 2), Block(foo), Block(bar), External(qux)]
+/// Roots: [0]
 /// ```
 ///
-/// The first three nodes build a tree, since the `Join` node references index 1 and 2. This
-/// tree is discovered by starting at index 0 and following all children until we reach terminal
-/// nodes (like `Block`s) and build up a stack of the discovered, but unvisited nodes.
+/// The only root is the `Join` node at index 0. The first three nodes of the forest form a tree,
+/// since the `Join` node references index 1 and 2. This tree is discovered by starting at
+/// index 0 and following all children until we reach terminal nodes (like `Block`s) and build up a
+/// stack of the discovered, but unvisited nodes. The stack is built such that popping elements off
+/// the stack (from the back) yields a postorder.
 ///
-/// After the first tree is discovered, the stack looks like this: `[Join, bar, foo]`. On each
+/// After the first tree is discovered, the stack looks like this: `[0, 2, 1]`. On each
 /// call to `next` one element is popped off this stack and returned.
 ///
-/// If the stack is exhausted we start another discovery if more unvisited nodes exist (e.g. the
-/// `External` node) and discover its tree (which is just itself).
+/// If the stack is exhausted we start another discovery if more unvisited roots exist. Since the
+/// `External` node is not a root and not referenced by any other tree in the forest, it will not be
+/// visited.
 ///
 /// The iteration on a high-level thus consists of a constant back and forth between discovering
 /// trees and returning nodes from the stack.
@@ -31,11 +36,11 @@ use crate::mast::{MastForest, MastNode, MastNodeId};
 pub(crate) struct MastForestDfsIter<'forest> {
     /// The forest that we're iterating.
     pub mast_forest: &'forest MastForest,
-    /// The index at which we last started a tree discovery.
+    /// The procedure root index at which we last started a tree discovery.
     ///
-    /// It is guaranteed that this value iterates through 0..mast_forest.num_nodes() eventually
-    /// which in turn guarantees that we visit all nodes.
-    pub last_tree_root_idx: u32,
+    /// This value iterates through 0..mast_forest.num_procedures() which guarantees that we visit
+    /// all nodes reachable from all roots.
+    pub last_procedure_root_idx: u32,
     /// Describes whether the node at some index has already been visited. Note that this is set to
     /// true for all nodes on the stack, even if the caller of the iterator has not yet seen the
     /// node. See [`Self::visit_later`] for more details.
@@ -51,7 +56,7 @@ impl<'forest> MastForestDfsIter<'forest> {
 
         Self {
             mast_forest,
-            last_tree_root_idx: 0,
+            last_procedure_root_idx: 0,
             node_visited: visited,
             unvisited_node_stack: Vec::new(),
         }
@@ -77,60 +82,62 @@ impl<'forest> MastForestDfsIter<'forest> {
         }
     }
 
-    /// Discovers a tree starting at the given index.
-    fn discover_tree(&mut self, idx: MastNodeId) {
-        let current_node = &self.mast_forest.nodes[idx.as_usize()];
+    /// Discovers a tree starting at the given root index.
+    fn discover_tree(&mut self, root_idx: MastNodeId) {
+        let current_node = &self.mast_forest.nodes[root_idx.as_usize()];
         // Note that the order in which we add or discover nodes is the reverse of postorder, since
         // we're pushing them onto a stack, which reverses the order itself. Hence, reversing twice
         // gives us the actual postorder we want.
         match current_node {
             MastNode::Block(_) => {
-                self.mark_for_visit(idx);
+                self.mark_for_visit(root_idx);
             },
             MastNode::Join(join_node) => {
-                self.mark_for_visit(idx);
+                self.mark_for_visit(root_idx);
                 self.discover_tree(join_node.second());
                 self.discover_tree(join_node.first());
             },
             MastNode::Split(split_node) => {
-                self.mark_for_visit(idx);
+                self.mark_for_visit(root_idx);
                 self.discover_tree(split_node.on_false());
                 self.discover_tree(split_node.on_true());
             },
             MastNode::Loop(loop_node) => {
-                self.mark_for_visit(idx);
+                self.mark_for_visit(root_idx);
                 self.discover_tree(loop_node.body());
             },
             MastNode::Call(call_node) => {
-                self.mark_for_visit(idx);
+                self.mark_for_visit(root_idx);
                 self.discover_tree(call_node.callee());
             },
             MastNode::Dyn(_) => {
-                self.mark_for_visit(idx);
+                self.mark_for_visit(root_idx);
             },
             MastNode::External(_) => {
-                self.mark_for_visit(idx);
+                self.mark_for_visit(root_idx);
             },
         }
     }
 
-    /// Finds the next unvisited node and discovers a tree from it.
+    /// Finds the next unvisited procedure root and discovers a tree from it.
     ///
     /// If the unvisited node stack is empty after calling this function, the iteration is complete.
     fn discover_nodes(&mut self) {
-        if self.mast_forest.nodes.is_empty() {
+        if self.mast_forest.num_procedures() == 0 {
             return;
         }
 
-        while self.node_visited[self.last_tree_root_idx as usize] {
-            if self.last_tree_root_idx + 1 >= self.mast_forest.num_nodes() {
+        let procedure_roots = self.mast_forest.procedure_roots();
+        // Find the next unvisited procedure root.
+        while self.node_visited[procedure_roots[self.last_procedure_root_idx as usize].as_usize()] {
+            if self.last_procedure_root_idx + 1 >= self.mast_forest.num_procedures() {
                 return;
             }
-            self.last_tree_root_idx += 1;
+
+            self.last_procedure_root_idx += 1;
         }
 
-        let tree_root_id = MastNodeId::from_u32_safe(self.last_tree_root_idx, self.mast_forest)
-            .expect("the index should never be incremented beyond the upper bound of tree nodes");
+        let tree_root_id = procedure_roots[self.last_procedure_root_idx as usize];
         self.discover_tree(tree_root_id);
     }
 }
@@ -151,8 +158,7 @@ impl<'forest> Iterator for MastForestDfsIter<'forest> {
         if !self.unvisited_node_stack.is_empty() {
             self.next()
         } else {
-            // If the stack is empty after node discovery, all nodes have been visited.
-            debug_assert!(self.node_visited.iter().all(|visited| *visited));
+            // If the stack is empty after tree discovery, all (reachable) nodes have been visited.
             None
         }
     }
@@ -177,32 +183,31 @@ mod tests {
 
     #[test]
     fn mast_forest_dfs() {
-        let node5_digest = random_digest();
+        let node0_digest = random_digest();
         let node1_digest = random_digest();
         let node2_digest = random_digest();
         let node3_digest = random_digest();
 
         let mut forest = MastForest::new();
-        // This will become the node at index 5 later.
-        let mut id5 = forest.add_external(node5_digest).unwrap();
+        forest.add_external(node0_digest).unwrap();
         let id1 = forest.add_external(node1_digest).unwrap();
         let id2 = forest.add_external(node2_digest).unwrap();
         let id3 = forest.add_external(node3_digest).unwrap();
         let id_split = forest.add_split(id2, id3).unwrap();
-        let mut id_join = forest.add_join(id2, id_split).unwrap();
+        let id_join = forest.add_join(id2, id_split).unwrap();
 
-        // Swap the join node to the first position so we visit a node first whose descendants have
-        // not been visited yet.
-        forest.nodes.swap(id5.as_usize(), id_join.as_usize());
-        std::mem::swap(&mut id5, &mut id_join);
+        forest.make_root(id_join);
+        forest.make_root(id1);
 
+        // Note that the node at index 0 is not visited because it is not reachable from any root
+        // and is not a root itself.
         let mut iterator = MastForestDfsIter::new(&forest);
+        // Node at id2 should only be visited once.
         assert_matches!(iterator.next().unwrap(), (id, MastNode::External(digest)) if digest.digest() == node2_digest && id == id2);
         assert_matches!(iterator.next().unwrap(), (id, MastNode::External(digest)) if digest.digest() == node3_digest && id == id3);
         assert_matches!(iterator.next().unwrap(), (id, MastNode::Split(_)) if id == id_split);
         assert_matches!(iterator.next().unwrap(), (id, MastNode::Join(_)) if id == id_join);
         assert_matches!(iterator.next().unwrap(), (id, MastNode::External(digest)) if digest.digest() == node1_digest&& id == id1);
-        assert_matches!(iterator.next().unwrap(), (id, MastNode::External(digest)) if digest.digest() == node5_digest&& id == id5);
         assert!(iterator.next().is_none());
     }
 }
