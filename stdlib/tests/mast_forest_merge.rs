8b6e66d81114713495c979a9b53d79ab81c5a285
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2024-10-24 16:26:54 +0200

    chore(stdlib): Add mast forest merge test

---
+++ stdlib/tests/mast_forest_merge.rs
@@ -0,0 +1,18 @@
+use processor::MastForest;
+
+/// Tests that the stdlib merged with itself produces a forest that has the same procedure
+/// roots.
+///
+/// This test is added here since we do not have the StdLib in miden-core where merging is
+/// implemented and the StdLib serves as a convenient example of a large MastForest.
+#[test]
+fn mast_forest_merge_stdlib() {
+    let std_forest = miden_stdlib::StdLibrary::default().mast_forest().as_ref().clone();
+
+    let (merged, _) = MastForest::merge([&std_forest, &std_forest]).unwrap();
+
+    let merged_digests = merged.procedure_digests().collect::<Vec<_>>();
+    for digest in std_forest.procedure_digests() {
+        assert!(merged_digests.contains(&digest));
+    }
+}
