b85121356bebda7f18424d2cfed98da7e6f8058a
Philippe Laferriere <plafer@protonmail.com>
2024-11-05 14:30:18 -0500

    fix: return error when 2 memory accesses at the same address in same clock cycle

--- processor/src/operations/io_ops.rs
+++ processor/src/operations/io_ops.rs
@@ -90,7 +90,7 @@ where
         let addr = Self::get_valid_address(self.stack.get(12))?;
 
         // load two words from memory
-        let words = self.chiplets.read_mem_double(ctx, addr);
+        let words = self.chiplets.read_mem_double(ctx, addr)?;
 
         // replace the stack elements with the elements from memory (in stack order)
         for (i, &mem_value) in words.iter().flat_map(|word| word.iter()).rev().enumerate() {
@@ -129,7 +129,7 @@ where
         let word = [self.stack.get(4), self.stack.get(3), self.stack.get(2), self.stack.get(1)];
 
         // write the word to memory and get the previous word
-        self.chiplets.write_mem(ctx, addr, word);
+        self.chiplets.write_mem(ctx, addr, word)?;
 
         // reverse the order of the memory word & update the stack state
         for (i, &value) in word.iter().rev().enumerate() {
@@ -160,7 +160,7 @@ where
         let value = self.stack.get(1);
 
         // write the value to the memory and get the previous word
-        let mut old_word = self.chiplets.write_mem_element(ctx, addr, value);
+        let mut old_word = self.chiplets.write_mem_element(ctx, addr, value)?;
         // put the retrieved word into stack order
         old_word.reverse();
 
@@ -192,7 +192,7 @@ where
         let words = self.host.borrow_mut().pop_adv_stack_dword(self)?;
 
         // write the words memory
-        self.chiplets.write_mem_double(ctx, addr, words);
+        self.chiplets.write_mem_double(ctx, addr, words)?;
 
         // replace the elements on the stack with the word elements (in stack order)
         for (i, &adv_value) in words.iter().flat_map(|word| word.iter()).rev().enumerate() {
@@ -252,7 +252,7 @@ where
     pub(crate) fn read_mem_word(&mut self, addr: Felt) -> Result<Word, ExecutionError> {
         let ctx = self.system.ctx();
         let mem_addr = Self::get_valid_address(addr)?;
-        let word_at_addr = self.chiplets.read_mem(ctx, mem_addr);
+        let word_at_addr = self.chiplets.read_mem(ctx, mem_addr)?;
 
         Ok(word_at_addr)
     }
