b1354c15865823e9f55ec2910e4b4073886e2981
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-12-20 16:49:11 +0200

    test: remove test deserializing a Miden package example build by the compiler
    Reasons:
    - The `Package` serialization proptests and testing `LibraryPath`,
    `ProcedureName` with example names produced by the compiler are enough;
    - The test is fragile and brakes on any change to `Package` underlying
      types serialization. Updating the `masp` file is non-trivial;

--- package/src/tests.rs
+++ package/src/tests.rs
@@ -7,20 +7,6 @@ use proptest::{
 
 use super::*;
 
-#[ignore = "update the binary atfer the changes in the Miden package format are settled"]
-#[test]
-fn basic_wallet_package_deserialization() {
-    // Test for the https://github.com/0xPolygonMiden/compiler/issues/347
-    // The included Miden package file is built at
-    // https://github.com/0xPolygonMiden/compiler/blob/6cd29e17b34c5abef7f6328c33af06f8bf203344/tests/integration/src/rust_masm_tests/rust_sdk.rs#L48-L63
-
-    let bytes = include_bytes!("../tests/data/basic_wallet.masp");
-
-    let package = Package::read_from_bytes(bytes).unwrap();
-    // dbg!(&package.manifest);
-    assert_eq!(package.name, "basic_wallet");
-}
-
 #[test]
 fn package_serialization_roundtrip() {
     let cases = 128; // since the test is quite expensive, 128 cases should be enough to cover all edge cases
