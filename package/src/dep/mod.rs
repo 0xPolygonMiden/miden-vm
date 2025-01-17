ab26b552c6ddf6f4c80e4b94d6b8e466b14b5fc3
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2025-01-17 10:44:23 +0200

    refactor: remove `SystemLibraryId` and use `DependencyName(String)`

--- package/src/dep/mod.rs
+++ package/src/dep/mod.rs
@@ -7,51 +7,10 @@ use crate::Digest;
 
 pub(crate) mod resolver;
 
-/// A system library identifier
-#[derive(
-    Debug, Copy, Clone, PartialEq, Eq, serde_repr::Serialize_repr, serde_repr::Deserialize_repr,
-)]
-#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
-#[repr(u8)]
-pub enum SystemLibraryId {
-    /// The standard library
-    Stdlib,
-    /// The base library
-    Miden,
-}
-
-impl core::str::FromStr for SystemLibraryId {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            // Compiler uses "std" and "base" to identify the standard and base libraries
-            // respectively. We also accept "stdlib" and "miden" as aliases for these libraries.
-            "std" | "stdlib" => Ok(Self::Stdlib),
-            "base" | "miden" => Ok(Self::Miden),
-            _ => Err(()),
-        }
-    }
-}
-
 /// The name of a dependency
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, derive_more::From)]
 #[cfg_attr(test, derive(proptest_derive::Arbitrary))]
-pub enum DependencyName {
-    /// The dependency is a system library
-    System(SystemLibraryId),
-    /// The dependency is a user library with the given name
-    User(String),
-}
-
-impl From<String> for DependencyName {
-    fn from(s: String) -> Self {
-        if let Ok(id) = s.parse() {
-            DependencyName::System(id)
-        } else {
-            DependencyName::User(s)
-        }
-    }
-}
+pub struct DependencyName(String);
 
 /// A package dependency
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
