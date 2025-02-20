533f5fa974be1e4211abbb520ca1368bd4cb1d19
Philippe Laferrière <plafer@protonmail.com>
2025-02-20 09:47:11 -0500

    Fix chiplets bus (#1664)
    * feat: implement `BusMessage` and `BusDebugger`
    
    * feat: integrate `BusDebugger` in `AuxColumnBuilder`
    
    * feat: implement messages for `BusColumnBuilder`
    
    * feat: add messages to bus debugger
    
    * fix: remove some `#[cfg(any(test, feature = "testing"))`
    
    * chore: update chiplets bus var names for mrupdate
    
    * test: add failing tests with `mrupdate` and `mpverify`
    
    * fix bus
    
    * fix clippy
    
    * chore: put bus debugger behind new `testing-slow` feature
    
    * changelog
    
    * fix: typo
    
    * fix bitwise message source
    
    * docs: fix `BusDebugger` docstring
    
    * nits
    
    * chore: rename `slow-testing` feature to `bus-debugger`
    
    * nit Cargo
    
    * rename to `Memory{Element,Word}Message`
    
    * clippy
    
    * chore: move bus builder to different files
    
    * chore: move messages into their own file

---
+++ processor/src/chiplets/aux_trace/bus/memory.rs
@@ -0,0 +1,420 @@
+use alloc::boxed::Box;
+use core::fmt::{Display, Formatter, Result as FmtResult};
+
+use miden_air::{
+    trace::{
+        chiplets::memory::{
+            MEMORY_ACCESS_ELEMENT, MEMORY_ACCESS_WORD, MEMORY_READ_ELEMENT_LABEL,
+            MEMORY_READ_WORD_LABEL, MEMORY_WRITE_ELEMENT_LABEL, MEMORY_WRITE_WORD_LABEL,
+        },
+        main_trace::MainTrace,
+    },
+    RowIndex,
+};
+use vm_core::{Felt, FieldElement, ONE, ZERO};
+
+use super::build_value;
+use crate::debug::{BusDebugger, BusMessage};
+
+// CONSTANTS
+// ================================================================================================
+
+const FOUR: Felt = Felt::new(4);
+
+// REQUESTS
+// ================================================================================================
+
+/// Builds `MLOADW` and `MSTOREW` requests made to the memory chiplet.
+pub(super) fn build_mem_mloadw_mstorew_request<E: FieldElement<BaseField = Felt>>(
+    main_trace: &MainTrace,
+    op_label: u8,
+    alphas: &[E],
+    row: RowIndex,
+    _debugger: &mut BusDebugger<E>,
+) -> E {
+    let word = [
+        main_trace.stack_element(3, row + 1),
+        main_trace.stack_element(2, row + 1),
+        main_trace.stack_element(1, row + 1),
+        main_trace.stack_element(0, row + 1),
+    ];
+    let addr = main_trace.stack_element(0, row);
+
+    debug_assert!(op_label == MEMORY_READ_WORD_LABEL || op_label == MEMORY_WRITE_WORD_LABEL);
+    let ctx = main_trace.ctx(row);
+    let clk = main_trace.clk(row);
+
+    let message = MemoryWordMessage {
+        op_label: Felt::from(op_label),
+        ctx,
+        addr,
+        clk,
+        word,
+        source: if op_label == MEMORY_READ_WORD_LABEL {
+            "mloadw"
+        } else {
+            "mstorew"
+        },
+    };
+
+    let value = message.value(alphas);
+
+    #[cfg(any(test, feature = "bus-debugger"))]
+    _debugger.add_request(Box::new(message), alphas);
+
+    value
+}
+
+/// Builds `MLOAD` and `MSTORE` requests made to the memory chiplet.
+pub(super) fn build_mem_mload_mstore_request<E: FieldElement<BaseField = Felt>>(
+    main_trace: &MainTrace,
+    op_label: u8,
+    alphas: &[E],
+    row: RowIndex,
+    _debugger: &mut BusDebugger<E>,
+) -> E {
+    let element = main_trace.stack_element(0, row + 1);
+    let addr = main_trace.stack_element(0, row);
+
+    debug_assert!(op_label == MEMORY_READ_ELEMENT_LABEL || op_label == MEMORY_WRITE_ELEMENT_LABEL);
+
+    let ctx = main_trace.ctx(row);
+    let clk = main_trace.clk(row);
+
+    let message = MemoryElementMessage {
+        op_label: Felt::from(op_label),
+        ctx,
+        addr,
+        clk,
+        element,
+    };
+
+    let value = message.value(alphas);
+
+    #[cfg(any(test, feature = "bus-debugger"))]
+    _debugger.add_request(Box::new(message), alphas);
+
+    value
+}
+
+/// Builds `MSTREAM` requests made to the memory chiplet.
+pub(super) fn build_mstream_request<E: FieldElement<BaseField = Felt>>(
+    main_trace: &MainTrace,
+    alphas: &[E],
+    row: RowIndex,
+    _debugger: &mut BusDebugger<E>,
+) -> E {
+    let op_label = Felt::from(MEMORY_READ_WORD_LABEL);
+    let addr = main_trace.stack_element(12, row);
+    let ctx = main_trace.ctx(row);
+    let clk = main_trace.clk(row);
+
+    let mem_req_1 = MemoryWordMessage {
+        op_label,
+        ctx,
+        addr,
+        clk,
+        word: [
+            main_trace.stack_element(7, row + 1),
+            main_trace.stack_element(6, row + 1),
+            main_trace.stack_element(5, row + 1),
+            main_trace.stack_element(4, row + 1),
+        ],
+        source: "mstream req 1",
+    };
+    let mem_req_2 = MemoryWordMessage {
+        op_label,
+        ctx,
+        addr: addr + FOUR,
+        clk,
+        word: [
+            main_trace.stack_element(3, row + 1),
+            main_trace.stack_element(2, row + 1),
+            main_trace.stack_element(1, row + 1),
+            main_trace.stack_element(0, row + 1),
+        ],
+        source: "mstream req 2",
+    };
+
+    let combined_value = mem_req_1.value(alphas) * mem_req_2.value(alphas);
+
+    #[cfg(any(test, feature = "bus-debugger"))]
+    {
+        _debugger.add_request(Box::new(mem_req_1), alphas);
+        _debugger.add_request(Box::new(mem_req_2), alphas);
+    }
+
+    combined_value
+}
+
+/// Builds `PIPE` requests made to the memory chiplet.
+pub(super) fn build_pipe_request<E: FieldElement<BaseField = Felt>>(
+    main_trace: &MainTrace,
+    alphas: &[E],
+    row: RowIndex,
+    _debugger: &mut BusDebugger<E>,
+) -> E {
+    let op_label = Felt::from(MEMORY_WRITE_WORD_LABEL);
+    let addr = main_trace.stack_element(12, row);
+    let ctx = main_trace.ctx(row);
+    let clk = main_trace.clk(row);
+
+    let mem_req_1 = MemoryWordMessage {
+        op_label,
+        ctx,
+        addr,
+        clk,
+        word: [
+            main_trace.stack_element(7, row + 1),
+            main_trace.stack_element(6, row + 1),
+            main_trace.stack_element(5, row + 1),
+            main_trace.stack_element(4, row + 1),
+        ],
+        source: "pipe req 1",
+    };
+    let mem_req_2 = MemoryWordMessage {
+        op_label,
+        ctx,
+        addr: addr + FOUR,
+        clk,
+        word: [
+            main_trace.stack_element(3, row + 1),
+            main_trace.stack_element(2, row + 1),
+            main_trace.stack_element(1, row + 1),
+            main_trace.stack_element(0, row + 1),
+        ],
+        source: "pipe req 2",
+    };
+
+    let combined_value = mem_req_1.value(alphas) * mem_req_2.value(alphas);
+
+    #[cfg(any(test, feature = "bus-debugger"))]
+    {
+        _debugger.add_request(Box::new(mem_req_1), alphas);
+        _debugger.add_request(Box::new(mem_req_2), alphas);
+    }
+
+    combined_value
+}
+
+/// Builds `RCOMBBASE` requests made to the memory chiplet.
+pub(super) fn build_rcomb_base_request<E: FieldElement<BaseField = Felt>>(
+    main_trace: &MainTrace,
+    alphas: &[E],
+    row: RowIndex,
+    _debugger: &mut BusDebugger<E>,
+) -> E {
+    let tz0 = main_trace.helper_register(0, row);
+    let tz1 = main_trace.helper_register(1, row);
+    let tzg0 = main_trace.helper_register(2, row);
+    let tzg1 = main_trace.helper_register(3, row);
+    let a0 = main_trace.helper_register(4, row);
+    let a1 = main_trace.helper_register(5, row);
+    let z_ptr = main_trace.stack_element(13, row);
+    let a_ptr = main_trace.stack_element(14, row);
+    let op_label = Felt::from(MEMORY_READ_WORD_LABEL);
+
+    let ctx = main_trace.ctx(row);
+    let clk = main_trace.clk(row);
+
+    let mem_req_1 = MemoryWordMessage {
+        op_label,
+        ctx,
+        addr: z_ptr,
+        clk,
+        word: [tz0, tz1, tzg0, tzg1],
+        source: "rcombbase req 1",
+    };
+    let mem_req_2 = MemoryWordMessage {
+        op_label,
+        ctx,
+        addr: a_ptr,
+        clk,
+        word: [a0, a1, ZERO, ZERO],
+        source: "rcombbase req 2",
+    };
+
+    let combined_value = mem_req_1.value(alphas) * mem_req_2.value(alphas);
+
+    #[cfg(any(test, feature = "bus-debugger"))]
+    {
+        _debugger.add_request(Box::new(mem_req_1), alphas);
+        _debugger.add_request(Box::new(mem_req_2), alphas);
+    }
+
+    combined_value
+}
+
+// RESPONSES
+// ================================================================================================
+
+/// Builds the response from the memory chiplet at `row`.
+pub(super) fn build_memory_chiplet_responses<E>(
+    main_trace: &MainTrace,
+    row: RowIndex,
+    alphas: &[E],
+    _debugger: &mut BusDebugger<E>,
+) -> E
+where
+    E: FieldElement<BaseField = Felt>,
+{
+    let access_type = main_trace.chiplet_selector_4(row);
+    let op_label = {
+        let is_read = main_trace.chiplet_selector_3(row);
+        get_memory_op_label(is_read, access_type)
+    };
+    let ctx = main_trace.chiplet_memory_ctx(row);
+    let clk = main_trace.chiplet_memory_clk(row);
+    let addr = {
+        let word = main_trace.chiplet_memory_word(row);
+        let idx0 = main_trace.chiplet_memory_idx0(row);
+        let idx1 = main_trace.chiplet_memory_idx1(row);
+
+        word + idx1.mul_small(2) + idx0
+    };
+
+    let message: Box<dyn BusMessage<E>> = if access_type == MEMORY_ACCESS_ELEMENT {
+        let idx0 = main_trace.chiplet_memory_idx0(row);
+        let idx1 = main_trace.chiplet_memory_idx1(row);
+
+        let element = if idx1 == ZERO && idx0 == ZERO {
+            main_trace.chiplet_memory_value_0(row)
+        } else if idx1 == ZERO && idx0 == ONE {
+            main_trace.chiplet_memory_value_1(row)
+        } else if idx1 == ONE && idx0 == ZERO {
+            main_trace.chiplet_memory_value_2(row)
+        } else if idx1 == ONE && idx0 == ONE {
+            main_trace.chiplet_memory_value_3(row)
+        } else {
+            panic!("Invalid word indices. idx0: {idx0}, idx1: {idx1}");
+        };
+
+        let message = MemoryElementMessage { op_label, ctx, addr, clk, element };
+
+        Box::new(message)
+    } else if access_type == MEMORY_ACCESS_WORD {
+        let value0 = main_trace.chiplet_memory_value_0(row);
+        let value1 = main_trace.chiplet_memory_value_1(row);
+        let value2 = main_trace.chiplet_memory_value_2(row);
+        let value3 = main_trace.chiplet_memory_value_3(row);
+
+        let message = MemoryWordMessage {
+            op_label,
+            ctx,
+            addr,
+            clk,
+            word: [value0, value1, value2, value3],
+            source: "memory chiplet",
+        };
+
+        Box::new(message)
+    } else {
+        panic!("Invalid memory element/word column value: {access_type}");
+    };
+
+    let value = message.value(alphas);
+
+    #[cfg(any(test, feature = "bus-debugger"))]
+    _debugger.add_response(message, alphas);
+
+    value
+}
+
+// HELPER FUNCTIONS
+// ================================================================================================
+
+/// Returns the operation unique label for memory operations.
+///
+/// The memory operation label is currently the only label that is built differently (or *simpler*)
+/// from the other chiplets. We should refactor the other chiplets to use a similar (simpler)
+/// approach.
+fn get_memory_op_label(is_read: Felt, is_word_access: Felt) -> Felt {
+    const MEMORY_SELECTOR: u8 = 0b110;
+    // Equivalent to `is_read << 1`
+    let is_read_left_shift_1 = is_read + is_read;
+
+    Felt::from(MEMORY_SELECTOR << 2) + is_read_left_shift_1 + is_word_access
+}
+
+// MESSAGES
+// ===============================================================================================
+
+pub struct MemoryWordMessage {
+    pub op_label: Felt,
+    pub ctx: Felt,
+    pub addr: Felt,
+    pub clk: Felt,
+    pub word: [Felt; 4],
+    pub source: &'static str,
+}
+
+impl<E> BusMessage<E> for MemoryWordMessage
+where
+    E: FieldElement<BaseField = Felt>,
+{
+    fn value(&self, alphas: &[E]) -> E {
+        alphas[0]
+            + build_value(
+                &alphas[1..9],
+                [
+                    self.op_label,
+                    self.ctx,
+                    self.addr,
+                    self.clk,
+                    self.word[0],
+                    self.word[1],
+                    self.word[2],
+                    self.word[3],
+                ],
+            )
+    }
+
+    fn source(&self) -> &str {
+        self.source
+    }
+}
+
+impl Display for MemoryWordMessage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(
+            f,
+            "{{ op_label: {}, ctx: {}, addr: {}, clk: {}, word: {:?} }}",
+            self.op_label, self.ctx, self.addr, self.clk, self.word
+        )
+    }
+}
+
+pub struct MemoryElementMessage {
+    pub op_label: Felt,
+    pub ctx: Felt,
+    pub addr: Felt,
+    pub clk: Felt,
+    pub element: Felt,
+}
+
+impl<E> BusMessage<E> for MemoryElementMessage
+where
+    E: FieldElement<BaseField = Felt>,
+{
+    fn value(&self, alphas: &[E]) -> E {
+        alphas[0]
+            + build_value(
+                &alphas[1..6],
+                [self.op_label, self.ctx, self.addr, self.clk, self.element],
+            )
+    }
+
+    fn source(&self) -> &str {
+        "memory element"
+    }
+}
+
+impl Display for MemoryElementMessage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(
+            f,
+            "{{ op_label: {}, ctx: {}, addr: {}, clk: {}, element: {} }}",
+            self.op_label, self.ctx, self.addr, self.clk, self.element
+        )
+    }
+}
---
+++ processor/src/chiplets/aux_trace/bus/mod.rs
@@ -0,0 +1,241 @@
+use bitwise::{build_bitwise_chiplet_responses, build_bitwise_request};
+use hasher::{
+    build_control_block_request, build_end_block_request, build_hasher_chiplet_responses,
+    build_hperm_request, build_mpverify_request, build_mrupdate_request,
+    build_respan_block_request, build_span_block_request, ControlBlockRequestMessage,
+};
+use kernel::{build_kernel_chiplet_responses, KernelRomMessage};
+use memory::{
+    build_mem_mload_mstore_request, build_mem_mloadw_mstorew_request,
+    build_memory_chiplet_responses, build_mstream_request, build_pipe_request,
+    build_rcomb_base_request, MemoryWordMessage,
+};
+use miden_air::{
+    trace::{
+        chiplets::{
+            hasher::LINEAR_HASH_LABEL,
+            memory::{
+                MEMORY_READ_ELEMENT_LABEL, MEMORY_READ_WORD_LABEL, MEMORY_WRITE_ELEMENT_LABEL,
+                MEMORY_WRITE_WORD_LABEL,
+            },
+        },
+        main_trace::MainTrace,
+    },
+    RowIndex,
+};
+use vm_core::{
+    ONE, OPCODE_CALL, OPCODE_DYN, OPCODE_DYNCALL, OPCODE_END, OPCODE_HPERM, OPCODE_JOIN,
+    OPCODE_LOOP, OPCODE_MLOAD, OPCODE_MLOADW, OPCODE_MPVERIFY, OPCODE_MRUPDATE, OPCODE_MSTORE,
+    OPCODE_MSTOREW, OPCODE_MSTREAM, OPCODE_PIPE, OPCODE_RCOMBBASE, OPCODE_RESPAN, OPCODE_SPAN,
+    OPCODE_SPLIT, OPCODE_SYSCALL, OPCODE_U32AND, OPCODE_U32XOR, ZERO,
+};
+
+use super::{Felt, FieldElement};
+use crate::{
+    debug::{BusDebugger, BusMessage},
+    trace::AuxColumnBuilder,
+};
+
+mod bitwise;
+mod hasher;
+mod kernel;
+mod memory;
+
+// BUS COLUMN BUILDER
+// ================================================================================================
+
+/// Describes how to construct the execution trace of the chiplets bus auxiliary trace column.
+#[derive(Default)]
+pub struct BusColumnBuilder {}
+
+impl<E: FieldElement<BaseField = Felt>> AuxColumnBuilder<E> for BusColumnBuilder {
+    /// Constructs the requests made by the VM-components to the chiplets at `row`.
+    fn get_requests_at(
+        &self,
+        main_trace: &MainTrace,
+        alphas: &[E],
+        row: RowIndex,
+        debugger: &mut BusDebugger<E>,
+    ) -> E
+    where
+        E: FieldElement<BaseField = Felt>,
+    {
+        let op_code_felt = main_trace.get_op_code(row);
+        let op_code = op_code_felt.as_int() as u8;
+
+        match op_code {
+            OPCODE_JOIN | OPCODE_SPLIT | OPCODE_LOOP | OPCODE_CALL => build_control_block_request(
+                main_trace,
+                main_trace.decoder_hasher_state(row),
+                op_code_felt,
+                alphas,
+                row,
+                debugger,
+            ),
+            OPCODE_DYN | OPCODE_DYNCALL => {
+                build_dyn_block_request(main_trace, op_code_felt, alphas, row, debugger)
+            },
+            OPCODE_SYSCALL => {
+                build_syscall_block_request(main_trace, op_code_felt, alphas, row, debugger)
+            },
+            OPCODE_SPAN => build_span_block_request(main_trace, alphas, row, debugger),
+            OPCODE_RESPAN => build_respan_block_request(main_trace, alphas, row, debugger),
+            OPCODE_END => build_end_block_request(main_trace, alphas, row, debugger),
+            OPCODE_U32AND => build_bitwise_request(main_trace, ZERO, alphas, row, debugger),
+            OPCODE_U32XOR => build_bitwise_request(main_trace, ONE, alphas, row, debugger),
+            OPCODE_MLOADW => build_mem_mloadw_mstorew_request(
+                main_trace,
+                MEMORY_READ_WORD_LABEL,
+                alphas,
+                row,
+                debugger,
+            ),
+            OPCODE_MSTOREW => build_mem_mloadw_mstorew_request(
+                main_trace,
+                MEMORY_WRITE_WORD_LABEL,
+                alphas,
+                row,
+                debugger,
+            ),
+            OPCODE_MLOAD => build_mem_mload_mstore_request(
+                main_trace,
+                MEMORY_READ_ELEMENT_LABEL,
+                alphas,
+                row,
+                debugger,
+            ),
+            OPCODE_MSTORE => build_mem_mload_mstore_request(
+                main_trace,
+                MEMORY_WRITE_ELEMENT_LABEL,
+                alphas,
+                row,
+                debugger,
+            ),
+            OPCODE_MSTREAM => build_mstream_request(main_trace, alphas, row, debugger),
+            OPCODE_RCOMBBASE => build_rcomb_base_request(main_trace, alphas, row, debugger),
+            OPCODE_HPERM => build_hperm_request(main_trace, alphas, row, debugger),
+            OPCODE_MPVERIFY => build_mpverify_request(main_trace, alphas, row, debugger),
+            OPCODE_MRUPDATE => build_mrupdate_request(main_trace, alphas, row, debugger),
+            OPCODE_PIPE => build_pipe_request(main_trace, alphas, row, debugger),
+            _ => E::ONE,
+        }
+    }
+
+    /// Constructs the responses from the chiplets to the other VM-components at `row`.
+    fn get_responses_at(
+        &self,
+        main_trace: &MainTrace,
+        alphas: &[E],
+        row: RowIndex,
+        debugger: &mut BusDebugger<E>,
+    ) -> E
+    where
+        E: FieldElement<BaseField = Felt>,
+    {
+        if main_trace.is_hash_row(row) {
+            build_hasher_chiplet_responses(main_trace, row, alphas, debugger)
+        } else if main_trace.is_bitwise_row(row) {
+            build_bitwise_chiplet_responses(main_trace, row, alphas, debugger)
+        } else if main_trace.is_memory_row(row) {
+            build_memory_chiplet_responses(main_trace, row, alphas, debugger)
+        } else if main_trace.is_kernel_row(row) {
+            build_kernel_chiplet_responses(main_trace, row, alphas, debugger)
+        } else {
+            E::ONE
+        }
+    }
+}
+
+// CHIPLETS REQUESTS TO MORE THAN ONE CHIPLET
+// ================================================================================================
+
+/// Builds requests made on a `DYN` or `DYNCALL` operation.
+fn build_dyn_block_request<E: FieldElement<BaseField = Felt>>(
+    main_trace: &MainTrace,
+    op_code_felt: Felt,
+    alphas: &[E],
+    row: RowIndex,
+    _debugger: &mut BusDebugger<E>,
+) -> E {
+    let control_block_req = ControlBlockRequestMessage {
+        transition_label: Felt::from(LINEAR_HASH_LABEL + 16),
+        addr_next: main_trace.addr(row + 1),
+        op_code: op_code_felt,
+        decoder_hasher_state: [ZERO; 8],
+    };
+
+    let memory_req = MemoryWordMessage {
+        op_label: Felt::from(MEMORY_READ_WORD_LABEL),
+        ctx: main_trace.ctx(row),
+        addr: main_trace.stack_element(0, row),
+        clk: main_trace.clk(row),
+        word: main_trace.decoder_hasher_state_first_half(row),
+        source: if op_code_felt == OPCODE_DYNCALL.into() {
+            "dyncall"
+        } else {
+            "dyn"
+        },
+    };
+
+    let combined_value = control_block_req.value(alphas) * memory_req.value(alphas);
+    #[cfg(any(test, feature = "bus-debugger"))]
+    {
+        use alloc::boxed::Box;
+        _debugger.add_request(Box::new(control_block_req), alphas);
+        _debugger.add_request(Box::new(memory_req), alphas);
+    }
+
+    combined_value
+}
+
+/// Builds requests made to kernel ROM chiplet when initializing a syscall block.
+fn build_syscall_block_request<E: FieldElement<BaseField = Felt>>(
+    main_trace: &MainTrace,
+    op_code_felt: Felt,
+    alphas: &[E],
+    row: RowIndex,
+    _debugger: &mut BusDebugger<E>,
+) -> E {
+    let control_block_req = ControlBlockRequestMessage {
+        transition_label: Felt::from(LINEAR_HASH_LABEL + 16),
+        addr_next: main_trace.addr(row + 1),
+        op_code: op_code_felt,
+        decoder_hasher_state: main_trace.decoder_hasher_state(row),
+    };
+
+    let kernel_rom_req = KernelRomMessage {
+        kernel_proc_digest: main_trace.decoder_hasher_state(row)[0..4].try_into().unwrap(),
+    };
+
+    let combined_value = control_block_req.value(alphas) * kernel_rom_req.value(alphas);
+
+    #[cfg(any(test, feature = "bus-debugger"))]
+    {
+        _debugger.add_request(alloc::boxed::Box::new(control_block_req), alphas);
+        _debugger.add_request(alloc::boxed::Box::new(kernel_rom_req), alphas);
+    }
+
+    combined_value
+}
+
+// HELPER FUNCTIONS
+// ================================================================================================
+
+/// Runs an inner product between the alphas and the elements.
+#[inline(always)]
+fn build_value<E: FieldElement<BaseField = Felt>, const N: usize>(
+    alphas: &[E],
+    elements: [Felt; N],
+) -> E {
+    debug_assert_eq!(alphas.len(), elements.len());
+    let mut value = E::ZERO;
+    for i in 0..N {
+        value += alphas[i].mul_base(elements[i]);
+    }
+    value
+}
+
+/// Returns the operation unique label.
+fn get_op_label(s0: Felt, s1: Felt, s2: Felt, s3: Felt) -> Felt {
+    s3.mul_small(1 << 3) + s2.mul_small(1 << 2) + s1.mul_small(2) + s0 + ONE
+}
