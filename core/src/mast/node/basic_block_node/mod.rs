2583b9d2d64a5f0996b7bc0d92628b1718102d73
Philippe Laferriere <plafer@proton.me>
2025-04-28 11:27:13 -0400

    revert `append_decorators`

--- core/src/mast/node/basic_block_node/mod.rs
+++ core/src/mast/node/basic_block_node/mod.rs
@@ -220,11 +220,11 @@ impl BasicBlockNode {
 
     /// Sets the provided list of decorators to be executed after all existing decorators.
     pub fn append_decorators(&mut self, decorator_ids: &[DecoratorId]) {
-        let mut new_decorators: DecoratorList =
-            decorator_ids.iter().map(|decorator_id| (0, *decorator_id)).collect();
-        new_decorators.extend(mem::take(&mut self.decorators));
+        let after_last_op_idx = self.num_operations() as usize;
 
-        self.decorators = new_decorators;
+        self.decorators.extend(
+            decorator_ids.into_iter().map(|&decorator_id| (after_last_op_idx, decorator_id)),
+        );
     }
 
     /// Used to initialize decorators for the [`BasicBlockNode`]. Replaces the existing decorators
