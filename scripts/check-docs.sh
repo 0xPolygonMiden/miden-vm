30bba1a35115f8eaea5007205b8e3ef0d5405ede
Himess <95512809+Himess@users.noreply.github.com>
2025-05-03 17:09:19 +0300

    fix:add docs check workflow and script from miden-base

---
+++ scripts/check-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -uo pipefail
+
+DOCS_DIR="docs/"
+
+if [ "${NO_DOCS_LABEL}" = "true" ]; then
+    echo "\"no docs\" label has been set"
+    exit 0
+else
+    if git diff --quiet "origin/${BASE_REF}" -- "${DOCS_DIR}"; then
+        >&2 echo "Changes should be accompanied by documentation updates in the \"docs/\" directory.
+This behavior can be overridden by using the \"no docs\" label, which is used for changes
+that don't require documentation updates or are purely maintenance-related."
+        exit 1
+    fi
+
+    echo "The \"docs/\" directory has been updated."
+fi
