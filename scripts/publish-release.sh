81b57cfe1a14fe7cc781ea30838a49416ff4506e
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2025-06-27 18:09:59 -0400

    release: update publish-release script to reflect new crate and dependency order

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Expects 
+# Expects
 # 1. to be run from the root of the repository
 # 2. for ~/.cargo/credentials.toml to contain your crates.io token (see
 #   https://doc.rust-lang.org/cargo/reference/publishing.html)
@@ -11,10 +11,11 @@
 
 cargo publish -p miden-core
 cargo publish -p miden-air
+cargo publish -p miden-assembly-syntax
+cargo publish -p miden-mast-package
 cargo publish -p miden-assembly
 cargo publish -p miden-processor
 cargo publish -p miden-prover
 cargo publish -p miden-verifier
 cargo publish -p miden-stdlib
-cargo publish -p miden-mast-package
 cargo publish -p miden-vm
